d3c2d07dffb32e776cf2d86556265f0e54845020
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-01 16:37:59 +0100

    First version.

---
+++ 2022/Day16/aoc16.py
@@ -0,0 +1,26 @@
+import sys
+import re
+
+
+class ValveNetwork:
+    def __init__(self, rawstr: str):
+        self.__adjlist: dict[str: tuple[int, list[str]]] = {}
+        for line in rawstr.splitlines():
+            valves = re.findall(r'[A-Z]{2}', line.strip('Valve'))
+            flow = int(re.findall(r'\d+', line)[0])
+            self.__adjlist[valves[0]] = (flow, valves[1:])
+
+    def getmaxflow(self) -> int:
+        return 1
+
+
+def main() -> int:
+    with open('testinput.txt', 'r') as file:
+        myvalves = ValveNetwork(file.read().strip('\n'))
+    p1 = myvalves.getmaxflow()
+    print("Part1: ", p1)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
