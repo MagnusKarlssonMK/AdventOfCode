39bbc22a625a2373dded72b6b249f7c5012f863e
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-12 23:17:23 +0100

    First version.

--- 2021/Day_15/aoc15.py
+++ 2021/Day_15/aoc15.py
@@ -1,27 +1,37 @@
 """
-
+Part 1: Pretty much a basic Djikstra exercise.
+Part 2: Expand the graph according to the updated rules and re-run the shortest-path calculation.
 """
 import sys
 from heapq import heappop, heappush
 
 
 class Cavegrid:
-    def __init__(self, griddata: list[str]):
-        self.grid = griddata
-        self.height = len(self.grid)
-        self.width = len(self.grid[0])
+    def __init__(self, griddata: list[str], expanded: bool = False):
+        grid = griddata
+        self.__height = len(grid)
+        self.__width = len(grid[0])
+        if expanded:
+            self.__height *= 5
+            self.__width *= 5
         self.__adj: dict[tuple[int, int]: list[tuple[int, int, int]]] = {}
-        for row in range(self.height):
-            for col in range(self.width):
+        for row in range(self.__height):
+            for col in range(self.__width):
                 self.__adj[(row, col)] = []
                 for direction in {(1, 0), (-1, 0), (0, 1), (0, -1)}:
-                    if 0 <= row + direction[0] < self.height and 0 <= col + direction[1] < self.width:
-                        self.__adj[(row, col)].append((row + direction[0], col + direction[1],
-                                                       int(self.grid[row + direction[0]][col + direction[1]])))
+                    if 0 <= row + direction[0] < self.__height and 0 <= col + direction[1] < self.__width:
+                        row_div = (row + direction[0]) // len(grid)
+                        row_mod = (row + direction[0]) % len(grid)
+                        col_div = (col + direction[1]) // len(grid[0])
+                        col_mod = (col + direction[1]) % len(grid[0])
+                        value = int(grid[row_mod][col_mod]) + row_div + col_div
+                        if value > 9:  # Note - wrap around to 1, not 0
+                            value -= 9
+                        self.__adj[(row, col)].append((row + direction[0], col + direction[1], value))
 
     def getminimumrisk(self) -> int:
         start = 0, 0
-        end = self.height - 1, self.width - 1
+        end = self.__height - 1, self.__width - 1
         costs = {start: 0}
         visited = set()
         queue = []
@@ -42,8 +52,11 @@ class Cavegrid:
 
 def main() -> int:
     with open('../Inputfiles/aoc15.txt', 'r') as file:
-        cave = Cavegrid(file.read().strip('\n').splitlines())
+        indata = file.read().strip('\n').splitlines()
+        cave = Cavegrid(indata)
+        largecave = Cavegrid(indata, True)
     print("Part 1:", cave.getminimumrisk())
+    print("Part 2:", largecave.getminimumrisk())
     return 0
 
 
