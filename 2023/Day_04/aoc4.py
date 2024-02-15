6168aec001581b0486855f8dace8172904e21a05
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-15 22:44:36 +0100

    Added description. Some cleanup.

--- 2023/Day_04/aoc4.py
+++ 2023/Day_04/aoc4.py
@@ -1,49 +1,41 @@
+"""
+Basically decodes the input into a Scratchcard class, and then counts the scores according to the rules.
+Having a class for the card is a bit over the top, I expected it to be beneficial for part 2 before seeing it.
+"""
 import sys
 import re
 
 
-class CardInpuError(Exception):
-    pass
-
-
 class Scratchcard:
     def __init__(self, inputstring: str):
-        if inputstring.count("Card ") == 1:
-            self.drawnumbers = []
-            cardstr, reststr = inputstring.split(":")
-            self.cardid = int(re.findall(r"\d+", cardstr)[0])
-            winstr, drawstr = reststr.split(" | ")
-            self.winningnumbers = list(map(int, [nbr for nbr in winstr.split()]))
-            self.drawnumbers = list(map(int, [nbr for nbr in drawstr.split()]))
-            self.wincount = len(set(self.winningnumbers) & set(self.drawnumbers))
-            self.score = 2 ** (self.wincount - 1) if self.wincount > 0 else 0
-        else:
-            raise CardInpuError
-
-    def __str__(self):
-        return f"CardID: {self.cardid} - Wincount: {self.wincount} - Score: {self.score}"
+        self.drawnumbers = []
+        cardstr, reststr = inputstring.split(":")
+        self.cardid = int(re.findall(r"\d+", cardstr)[0])
+        winstr, drawstr = reststr.split(" | ")
+        self.winningnumbers = list(map(int, [nbr for nbr in winstr.split()]))
+        self.drawnumbers = list(map(int, [nbr for nbr in drawstr.split()]))
+        self.wincount = len(set(self.winningnumbers) & set(self.drawnumbers))
+        self.score = 2 ** (self.wincount - 1) if self.wincount > 0 else 0
 
 
 def main() -> int:
     totalscore = 0
     totalnbrofcards = 0
-    copylist = [0] * 10
+    copylist = [0 for _ in range(10)]
 
     with open("../Inputfiles/aoc4.txt", "r") as file:
-        for line in file.readlines():
-            try:
-                newcard = Scratchcard(line.strip("\n"))
-                newcardcount = 1 + copylist.pop(0)
-                totalnbrofcards += newcardcount
-                copylist.append(0)
-                for copyidx in range(len(copylist)):
-                    if copyidx < newcard.wincount:
-                        copylist[copyidx] += newcardcount
-                totalscore += newcard.score
-            except CardInpuError:
-                print("Discarding line")
+        for line in file.read().strip('\n').splitlines():
+            newcard = Scratchcard(line)
+            newcardcount = 1 + copylist.pop(0)
+            totalnbrofcards += newcardcount
+            copylist.append(0)
+            for copyidx in range(len(copylist)):
+                if copyidx < newcard.wincount:
+                    copylist[copyidx] += newcardcount
+            totalscore += newcard.score
 
-    print("Total score (Part1): ", totalscore, " Nbr of cards (Part2): ", totalnbrofcards)
+    print("Part1:", totalscore)
+    print("Part2:", totalnbrofcards)
     return 0
 
 
