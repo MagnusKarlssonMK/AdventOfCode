02277f29fad742b9f4e29b5a9b42860fdce41d4d
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-16 13:17:44 +0100

    Major re-write, storing just coordinates instead of the entire grid. Execution time decreased from 1+1 seconds to 200ms total.

--- 2023/Day_11/aoc11.py
+++ 2023/Day_11/aoc11.py
@@ -1,67 +1,53 @@
+"""
+Stores the coordinates of galaxies in a Space class, and also generates lists of empty rows and columns. For Part 1,
+the distance is then calculated with the manhattan distance between each pair of galaxies, and for each pair also
+checking the number of empty rows/columns between them. Since the grid input doesn't change between Part 1 & 2 other
+than the scaling of empty space, we can store the result from Part 1 as [manhattan distance, nbr of empty spaces] and
+re-use that for a lightning fast Part 2.
+"""
 import sys
-
-RowCol = tuple[int, int]
-
-
-def getstardistance(star1: RowCol, star2: RowCol):
-    return abs(star1[0] - star2[0]) + abs(star1[1] - star2[1])
+import re
 
 
-class Grid:
-    def __init__(self, rawstr: str, expansionrate: int = 2):
-        self.grid: list[list[str]] = []
-        self.starlist: list[RowCol] = []
-        self.expansionrate = expansionrate
-        count = 0
-        for line in rawstr.splitlines():
-            count += 1
-            if len(line) > 1:
-                if line.find("#") >= 0:
-                    charlist = []
-                    for idx, c in enumerate(line):
-                        charlist.append(c)
-                        if c == "#":
-                            self.starlist.append((len(self.grid), idx))
-                    self.grid.append([c for c in line])
-                else:
-                    self.grid.append(['X'] * len(line))
-        # Replace every empty column with 'X'
-        for col in range(0, len(self.grid[0])):
-            if not any(row[col] == "#" for row in self.grid):
-                for row in self.grid:
-                    row[col] = 'X'
+class Space:
+    def __init__(self, spaceinput: list[str], e_rate: int = 2):
+        self.galaxies: list[tuple[int, int]] = []  # Row, Col
+        self.__expansionrate = e_rate
+        for idx, line in enumerate(spaceinput):
+            [self.galaxies.append((idx, g.start())) for g in re.finditer(r"#", line)]
+        rows = set([c[0] for c in self.galaxies])
+        cols = set([c[1] for c in self.galaxies])
+        self.__emptyrows = [r for r in range(max(rows)) if r not in rows]
+        self.__emptycols = [c for c in range(max(rows)) if c not in cols]
+        self.__distancecache = [-1, -1]
 
-    def setexpansionrate(self, newrate) -> None:
-        self.expansionrate = newrate
+    def set_expansionrate(self, newrate: int) -> None:
+        self.__expansionrate = newrate
 
-    def getdistancesum(self) -> int:
+    def get_distancesum(self) -> int:
         retval = 0
-        starqueue = self.starlist.copy()
-        while len(starqueue) > 0:
-            nextstar = starqueue.pop(0)
-            for neighborstar in starqueue:
-                for row in range(nextstar[0], neighborstar[0]):
-                    if self.grid[row][nextstar[1]] == 'X':
-                        retval += self.expansionrate
-                    else:
-                        retval += 1
-                for col in range(min(nextstar[1], neighborstar[1]), max(nextstar[1], neighborstar[1])):
-                    if self.grid[neighborstar[0]][col] == 'X':
-                        retval += self.expansionrate
-                    else:
-                        retval += 1
-        return retval
+        emptyspace = 0
+        if self.__distancecache[0] < 0:
+            for i in range(len(self.galaxies) - 1):
+                for j in range(i + 1, len(self.galaxies)):
+                    rowrange = (min(self.galaxies[i][0], self.galaxies[j][0]),
+                                max(self.galaxies[i][0], self.galaxies[j][0]))
+                    colrange = (min(self.galaxies[i][1], self.galaxies[j][1]),
+                                max(self.galaxies[i][1], self.galaxies[j][1]))
+                    emptyspace += (sum([1 for r in self.__emptyrows if rowrange[0] < r < rowrange[1]]) +
+                                   sum([1 for c in self.__emptycols if colrange[0] < c < colrange[1]]))
+                    retval += rowrange[1] - rowrange[0] + colrange[1] - colrange[0]
+            self.__distancecache[0] = retval
+            self.__distancecache[1] = emptyspace
+        return self.__distancecache[0] + (self.__expansionrate - 1) * self.__distancecache[1]
 
 
 def main() -> int:
-    with open("../Inputfiles/aoc11.txt", "r") as file:
-        mygrid = Grid(file.read())
-
-    result_p1 = mygrid.getdistancesum()
-    print("Part1: ", result_p1)
-    mygrid.setexpansionrate(1000000)
-    result_p2 = mygrid.getdistancesum()
-    print("Part2: ", result_p2)
+    with open('../Inputfiles/aoc11.txt', 'r') as file:
+        space = Space(file.read().strip('\n').splitlines())
+    print("Part 1:", space.get_distancesum())
+    space.set_expansionrate(1000000)
+    print("Part 2:", space.get_distancesum())
     return 0
 
 
