350734285c935842ead8f268cbe027bec8d622e9
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-17 23:28:55 +0100

    Restructured and cleaned up.

--- 2023/Day_19/aoc19.py
+++ 2023/Day_19/aoc19.py
@@ -1,15 +1,23 @@
-# AoC 2023 day 19
+"""
+Part 1 - Fairly straightforward, just set up a way to organize the workflows and rules and run the input through and
+see what comes out the other end.
+Part 2 - Yet another challenge in handling splitting ranges, mainly a headache trying to keep track of the layers of
+dicts, ranges, lists etc.
+"""
+import sys
 import re
+import operator
 from math import prod
+
 
 Partrating = dict[str: int]   # Parts = ['x', 'm', 'a', 's']
 Partratingrange = dict[str: range]
-Partgroup = dict[str: Partratingrange]
+Op = {'<': operator.lt, '>': operator.gt}
 
 
 class Rule:
-    def __init__(self, inputstr: str):
-        if len(tmp := inputstr.split(":")) > 1:
+    def __init__(self, rulestr: str):
+        if len(tmp := rulestr.split(":")) > 1:
             self.condpart, self.condoperation, nbrstr = re.findall(r"(\w)(.)(\d+)", tmp[0])[0]
             self.condthrshold = int(nbrstr)
         else:
@@ -20,105 +28,89 @@ class Rule:
 
     def getverdict(self, part: Partrating) -> str:
         if self.condpart:
-            if self.condoperation == ">":
-                if part[self.condpart] > self.condthrshold:
-                    return self.ifpass
-                else:
-                    return ""
+            if Op[self.condoperation](part[self.condpart], self.condthrshold):
+                return self.ifpass
             else:
-                if part[self.condpart] < self.condthrshold:
-                    return self.ifpass
-                else:
-                    return ""
+                return ""
         else:
             return self.ifpass
 
-    def getrangesplit(self, newrangelist):
+    def getrangesplit(self, inranges: Partratingrange) -> iter:
         if self.condpart:
-            if self.condthrshold in newrangelist[1][self.condpart]:
-                retsetlow = dict(newrangelist[1])
-                retsethigh = dict(newrangelist[1])
-                thold = self.condthrshold if self.condoperation == "<" else self.condthrshold + 1
-                retsetlow[self.condpart] = range(retsetlow[self.condpart].start, thold)
-                retsethigh[self.condpart] = range(thold, newrangelist[1][self.condpart].stop)
+            thrshold = self.condthrshold if self.condoperation == "<" else self.condthrshold + 1
+            if self.condthrshold in inranges[self.condpart]:
+                outrangeslow: Partratingrange = dict(inranges)
+                outrangeshigh: Partratingrange = dict(inranges)
+                outrangeslow[self.condpart] = range(outrangeslow[self.condpart].start, thrshold)
+                outrangeshigh[self.condpart] = range(thrshold, inranges[self.condpart].stop)
                 if self.condoperation == ">":
-                    return ("", retsetlow), (self.ifpass, retsethigh)
+                    yield "", outrangeslow
+                    yield self.ifpass, outrangeshigh
                 else:
-                    return ("", retsethigh), (self.ifpass, retsetlow)
+                    yield "", outrangeshigh
+                    yield self.ifpass, outrangeslow
             else:
-                print("Rule not in range, TBD")
-            return (self.ifpass, newrangelist[1]),
+                yield self.ifpass, inranges
         else:
-            return (self.ifpass, newrangelist[1]),
+            yield self.ifpass, inranges
 
-    def __str__(self):
-        return f"Condition: {self.condpart} {self.condoperation} {self.condthrshold} If pass: -{self.ifpass}-"
+    def __repr__(self):
+        return f"{self.condpart}-{self.condoperation}-{self.condthrshold}-{self.ifpass}"
 
 
-workflows: dict[str: list[Rule]] = {}
-result_p1 = 0
+class System:
+    def __init__(self, wfstr: str):
+        self.workflows: dict[str: list[Rule]] = {}
+        for flow in wfstr.splitlines():
+            label, rls = flow.strip('}').split('{')
+            self.workflows[label] = [Rule(r) for r in rls.split(',')]
 
-with open("../Inputfiles/aoc19.txt", "r") as file:
-    while len(line := file.readline()) > 1:
-        label, rules = line.strip("\n").strip("}").split("{")
-        rulelist = rules.split(",")
-        # print(label, rulelist)
-        workflows[label] = []
-        for rulestr in rulelist:
-            workflows[label].append(Rule(rulestr))
-
-    while len(line := file.readline()) > 1:
-        partlist: Partrating = {part: int(count) for part, count in re.findall(r"(.)=(\d+)", line)}
-        # print(partlist)
+    def process_workflow(self, rating: Partrating) -> int:
         currentworkflow = "in"
         while currentworkflow not in ("A", "R"):
-            for idx in range(len(workflows[currentworkflow])):
-                a: Rule = workflows[currentworkflow][idx]
-                b = a.getverdict(partlist)
-                if b != "":
-                    currentworkflow = b
+            for rule in self.workflows[currentworkflow]:
+                if (verdict := rule.getverdict(rating)) != "":
+                    currentworkflow = verdict
                     break
-
         if currentworkflow == "A":
-            result_p1 += sum(partlist.values())
+            return sum(rating.values())
+        return 0
 
-print("Part1: ", result_p1)
+    def get_combinationcount(self) -> int:
+        initialpartgroup: tuple[str, Partratingrange] = ("in", {"x": range(1, 4001), "m": range(1, 4001),
+                                                                "a": range(1, 4001), "s": range(1, 4001)})
+        queue: list[tuple[str, Partratingrange]] = [initialpartgroup]
+        verdict_a = []
 
-initialpartgroup = ("in", {"x": range(1, 4001), "m": range(1, 4001), "a": range(1, 4001), "s": range(1, 4001)})
+        while len(queue) > 0:
+            currentworkflow, ranges = queue.pop(0)
+            if currentworkflow not in ("A", "R"):
+                for rule in self.workflows[currentworkflow]:
+                    done = True
+                    for newranges in rule.getrangesplit(ranges):
+                        if newranges[0] != "":
+                            queue.append(newranges)
+                        else:
+                            done = False
+                            ranges = newranges[1]
+                    if done:
+                        break
+            elif currentworkflow == "A":
+                verdict_a.append(ranges)
 
-outerq = [initialpartgroup]
-verdict_A = []
-verdict_R = []
+        return sum([prod([r.stop - r.start for r in list(a_ranges.values())]) for a_ranges in verdict_a])
 
-while len(outerq) > 0:
-    currentgroup = outerq.pop()
-    while currentgroup[0] not in ("A", "R"):
-        currentworkflow = str(currentgroup[0])
-        for idx in range(len(workflows[currentworkflow])):
-            a: Rule = workflows[currentworkflow][idx]
-            b = a.getrangesplit(currentgroup)
-            if len(b) > 1:  # Funkar inte! Räknar inre element, så alltid större än 1
-                currentgroup = b[0]
-                outerq.append(b[1])
-            else:
-                currentgroup = b[0]
-            if currentgroup[0] != "":
-                break
-    if currentgroup[0] == "A":
-        verdict_A.append(currentgroup[1])
-    elif currentgroup[0] == "R":
-        verdict_R.append(currentgroup[1])
-    else:
-        print("Unexpected...???")
 
-result_p2 = 0
+def main() -> int:
+    with open('../Inputfiles/aoc19.txt') as file:
+        wf, rt = file.read().strip('\n').split('\n\n')
+    system = System(wf)
+    ratings: list[Partrating] = [{part: int(count) for part, count in re.findall(r"(.)=(\d+)", rating)}
+                                 for rating in rt.splitlines()]
+    print("Part 1:", sum([system.process_workflow(rating) for rating in ratings]))
+    print("Part 2:", system.get_combinationcount())
+    return 0
 
-for a_ranges in verdict_A:
-    result_p2 += prod(r.stop - r.start for r in list(a_ranges.values()))
 
-print("Part2: ", result_p2)
-
-# 167409079868000
-# 167807805646283
-# 21856640000000
-# 96000000000000
+if __name__ == "__main__":
+    sys.exit(main())
