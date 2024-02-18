1c8510bab58d21f25e01f8a4821ffe7036293b29
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-19 00:27:17 +0100

    Added description.

--- 2023/Day_25/aoc25.py
+++ 2023/Day_25/aoc25.py
@@ -1,3 +1,7 @@
+"""
+Uses the 'minimum cut' function from nx module to determine the answer.
+(Saving it for a rainy day to figure out how this actually works.)
+"""
 import sys
 import networkx as nx
 import math
@@ -7,24 +11,22 @@ def main() -> int:
     graph = nx.Graph()
 
     with open("../Inputfiles/aoc25.txt", "r") as file:
-        for line in file:
-            left, right = (part.strip() for part in line.split(":"))
+        for line in file.read().strip('\n').splitlines():
+            left, right = (part.strip() for part in line.split(": "))
             right_parts = right.split()
             graph.add_node(left)
             [graph.add_edge(left, r, capacity=1.0) for r in right_parts]
 
-    result_p1 = None
-    print(graph)
+    result_p1 = -1
     left = next(iter(graph.nodes))
     for right in graph.nodes:
         if left != right:
             cut_val, partitions = nx.minimum_cut(graph, left, right)
             if cut_val == 3:
-                print(partitions)
                 result_p1 = math.prod(len(partition) for partition in partitions)
                 break
 
-    print("Part1: ", result_p1)
+    print("Part1:", result_p1)
     return 0
 
 
