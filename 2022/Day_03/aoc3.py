fc412adfb6bad6c2141baaf746030b95fe0f2641
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-21 16:15:37 +0100

    Minor cleanup.

--- 2022/Day_03/aoc3.py
+++ 2022/Day_03/aoc3.py
@@ -8,25 +8,21 @@ def main() -> int:
     totalprio_p2 = 0
     with open('../Inputfiles/aoc3.txt', 'r') as file:
         linebuffer = []
-        for line in file.readlines():
-            if len(line) > 1:
-                line = line.strip('\n')
-                linebuffer.append(line)
-                left, right = line[:len(line) // 2], line[len(line) // 2:]
-                shared_p1 = ''.join(set(left).intersection(right))
-                if len(shared_p1) > 0:
-                    for r in rangelist:
-                        if ord(shared_p1[0]) in r[0]:
-                            totalprio_p1 += ord(shared_p1[0]) - r[0].start + r[1]
-                if len(linebuffer) >= 3:
-                    shared_p2 = ''.join(set(linebuffer[0]).intersection(linebuffer[1]).intersection(linebuffer[2]))
-                    linebuffer.clear()
-                    if len(shared_p2) > 0:
-                        for r in rangelist:
-                            if ord(shared_p2[0]) in r[0]:
-                                totalprio_p2 += ord(shared_p2[0]) - r[0].start + r[1]
-    print("Part1: ", totalprio_p1)
-    print("Part2: ", totalprio_p2)
+        for line in file.read().strip('\n').splitlines():
+            linebuffer.append(line)
+            left, right = line[:len(line) // 2], line[len(line) // 2:]
+            shared_p1 = ''.join(set(left).intersection(right))
+            if len(shared_p1) > 0:
+                totalprio_p1 += sum([ord(shared_p1[0]) - r[0].start + r[1] for r in rangelist
+                                     if ord(shared_p1[0]) in r[0]])
+            if len(linebuffer) >= 3:
+                shared_p2 = ''.join(set(linebuffer[0]).intersection(linebuffer[1]).intersection(linebuffer[2]))
+                linebuffer.clear()
+                if len(shared_p2) > 0:
+                    totalprio_p2 += sum([ord(shared_p2[0]) - r[0].start + r[1] for r in rangelist
+                                         if ord(shared_p2[0]) in r[0]])
+    print("Part1:", totalprio_p1)
+    print("Part2:", totalprio_p2)
 
     return 0
 
