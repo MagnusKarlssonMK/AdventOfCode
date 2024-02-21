8fdd451638806dcd32bb26b8ce178316eee68440
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-21 16:18:27 +0100

    Minor cleanup.

--- 2022/Day_04/aoc4.py
+++ 2022/Day_04/aoc4.py
@@ -4,13 +4,13 @@ import re
 
 def main() -> int:
     with open('../Inputfiles/aoc4.txt', 'r') as file:
-        lines = [list(map(int, re.split('[,-]', line))) for line in file.read().split("\n") if len(line) > 1]
+        lines = [list(map(int, re.split('[,-]', line))) for line in file.read().strip('\n').splitlines()]
 
     result_p1 = sum([1 for a, b, c, d in lines if a <= c <= d <= b or c <= a <= b <= d])
     result_p2 = sum([1 for a, b, c, d in lines if a <= d and c <= b])
 
-    print("Part1: ", result_p1)
-    print("Part2: ", result_p2)
+    print("Part1:", result_p1)
+    print("Part2:", result_p2)
     return 0
 
 
