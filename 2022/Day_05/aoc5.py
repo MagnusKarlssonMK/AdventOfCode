5779c34e8278eba6c61504c6ca2ccfbdda8e0b0f
BigMange <155587399+BigMange@users.noreply.github.com>
2024-02-21 19:09:54 +0100

    Restructured and a short description.

--- 2022/Day_05/aoc5.py
+++ 2022/Day_05/aoc5.py
@@ -1,43 +1,43 @@
+"""
+Stores the input data in a crateship class, containing the crates and procedures. The answer is provided with a
+method running the procedures. This is done on a local copy of the crate data to avoid having to create a copy
+of the entire class for Part 2.
+"""
 import sys
 import re
-import copy
+from copy import deepcopy
+
+
+class Crateship:
+    def __init__(self, cratestr: str, procedurestr: str):
+        self.procedures = [tuple(map(int, re.findall(r'\d+', row))) for row in procedurestr.splitlines()]
+        self.crates: dict[int: list[str]] = {}
+        cratelines = cratestr.splitlines()
+        for idx, line in enumerate(reversed(cratelines)):
+            if idx == 0:
+                for nbr in re.findall(r'\d', line):
+                    self.crates[int(nbr)] = []
+            else:
+                for m in re.finditer(r'\w', line):
+                    self.crates[int(cratelines[-1][m.start()])].append(line[m.start()])
+
+    def run_procedures(self, multicrates: bool = False) -> str:
+        crates = deepcopy(self.crates)
+        for proc in self.procedures:  # Procedures: Nbr - from - to
+            for idx in range(proc[0]):
+                if multicrates:
+                    movecrate = crates[proc[1]].pop(idx - proc[0])
+                else:
+                    movecrate = crates[proc[1]].pop()
+                crates[proc[2]].append(movecrate)
+        return ''.join([crates[key][-1] for key in list(crates.keys())])
 
 
 def main() -> int:
     with open('../Inputfiles/aoc5.txt', 'r') as file:
-        cratelines, procedureslines = file.read().strip('\n').split('\n\n')
-    procedures = [list(map(int, re.findall(r'\d+', row))) for row in procedureslines.splitlines()]
-    cratelines = cratelines.splitlines()
-    crates: dict[int: list[str]] = {}
-    for idx, line in enumerate(reversed(cratelines)):
-        if idx == 0:
-            for nbr in re.findall(r'\d', line):
-                crates[int(nbr)] = []
-        else:
-            for m in re.finditer(r'\w', line):
-                crates[int(cratelines[-1][m.start()])].append(line[m.start()])
-
-    crates_b = copy.deepcopy(crates)
-
-    for proc in procedures:  # Procedures: Nbr - from - to
-        for _ in range(proc[0]):
-            movecrate = crates[proc[1]].pop()
-            crates[proc[2]].append(movecrate)
-    result_p1 = ""
-    for key in list(crates.keys()):
-        result_p1 += crates[key][-1]
-    print("Part1: ", result_p1)
-
-    for proc in procedures:
-        for idx in range(proc[0]):
-            movecrate = crates_b[proc[1]].pop(idx - proc[0])
-            crates_b[proc[2]].append(movecrate)
-
-    result_p2 = ""
-    for key in list(crates_b.keys()):
-        result_p2 += crates_b[key][-1]
-    print("Part2: ", result_p2)
-
+        ship = Crateship(*file.read().strip('\n').split('\n\n'))
+    print("Part 1:", ship.run_procedures())
+    print("Part 2:", ship.run_procedures(True))
     return 0
 
 
