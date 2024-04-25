d8136011e57f2e9b1d6371194a3435813a751414
BigMange <155587399+BigMange@users.noreply.github.com>
2024-04-25 23:38:48 +0200

    Rewrite with an optimized solution.

--- 2021/Day_01/aoc1.py
+++ 2021/Day_01/aoc1.py
@@ -1,37 +1,20 @@
 """
-Part 1: Simply parse the input to a list, and loop through the list comparing elements and counting the number of times
-the depth increases.
-Part 2: Keep a buffer of 3 values and compare for each step and count the number of times the sum of the buffer
-increases from one time to the next.
+For part 2, we actually only need to compare the first element of the first window with the last element of the
+second window, since all the elements in between are shared by both. So by making use of that logic, the answer
+for both part 1 and 2 can be calculated with one function, taking the window length as an input.
 """
 import sys
 
 
-def count_depth_increase(nbrlist: list[int]) -> int:
-    """Returns the answer to Part 1."""
-    return sum([int(nbrlist[n] > nbrlist[n-1]) for n in range(1, len(nbrlist))])
-
-
-def count_window_depth_increase(nbrlist: list[int]) -> int:
-    """Returns the answer to Part 2."""
-    window_buffer = []
-    previous = 999999  # Initialize to something large to make sure we don't count the first window as an increase.
-    inc_count = 0
-    for n, _ in enumerate(nbrlist):
-        window_buffer.append(nbrlist[n])
-        if len(window_buffer) > 3:
-            window_buffer.pop(0)
-            if (newvalue := sum(window_buffer)) > previous:
-                inc_count += 1
-            previous = newvalue
-    return inc_count
+def count_depth_increase(nbrlist: list[int], windowsize: int) -> int:
+    return sum([1 for i in range(windowsize, len(nbrlist)) if nbrlist[i] > nbrlist[i - windowsize]])
 
 
 def main() -> int:
     with open('../Inputfiles/aoc1.txt', 'r') as file:
         numbers = list(map(int, file.read().strip('\n').splitlines()))
-    print(f"Part 1: {count_depth_increase(numbers)}")
-    print(f"Part 2: {count_window_depth_increase(numbers)}")
+    print(f"Part 1: {count_depth_increase(numbers, 1)}")
+    print(f"Part 2: {count_depth_increase(numbers, 3)}")
     return 0
 
 
