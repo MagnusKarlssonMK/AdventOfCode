3ca0d2d8540e4a72ab855172f63d56e373a8e98e
BigMange <155587399+BigMange@users.noreply.github.com>
2024-04-28 22:29:39 +0200

    Some minor refactoring.

--- 2021/Day_03/aoc3.py
+++ 2021/Day_03/aoc3.py
@@ -1,49 +1,43 @@
 import sys
 
 
-def get_most_and_least_common(inlist: list[str]) -> tuple[str, str]:
-    gamma = [0 for _, _ in enumerate(inlist[0])]
-    for line in inlist:
-        for i, c in enumerate(line):
-            gamma[i] += int(c)
-    for i, _ in enumerate(gamma):
-        gamma[i] = min(1, 2 * gamma[i] // len(inlist))  # There's got to be a more intuitive way to do this...
-    epsilon = [(i + 1) % 2 for i in gamma]
-    return "".join(list(map(str, gamma))), "".join(list(map(str, epsilon)))
-
-
 class Diagnostics:
     def __init__(self, rawstr: str):
         self.__lines = rawstr.splitlines()
-        self.__mostcommon = ""
-        self.__leastcommon = ""
+        self.__nbrbits = len(self.__lines[0])
+
+    def __get_most_and_least_common(self, inlist: list[str]) -> tuple[str, str]:
+        gamma = [0 for _ in range(self.__nbrbits)]
+        for line in inlist:
+            for i, c in enumerate(line):
+                gamma[i] += int(c)
+        for bit_idx in range(self.__nbrbits):
+            gamma[bit_idx] = min(1, 2 * gamma[bit_idx] // len(inlist))
+        epsilon = [(i + 1) % 2 for i in gamma]
+        return "".join(list(map(str, gamma))), "".join(list(map(str, epsilon)))
 
     def getpowerconsumption(self) -> int:
-        self.__mostcommon, self.__leastcommon = get_most_and_least_common(self.__lines)
-        return int(self.__mostcommon, 2) * int(self.__leastcommon, 2)
+        mostcommon, leastcommon = self.__get_most_and_least_common(self.__lines)
+        return int(mostcommon, 2) * int(leastcommon, 2)
 
     def getlifesupportrating(self) -> int:
         return self.__getrating(0) * self.__getrating(1)
 
     def __getrating(self, most_least: int) -> int:
-        nbrlist = self.__lines.copy()
-        for i, _ in enumerate(nbrlist[0]):
+        nbrlist = list(self.__lines)
+        for bit_idx in range(self.__nbrbits):
             if len(nbrlist) <= 1:
                 break
-            bufferlist = []
-            matchnbr = get_most_and_least_common(nbrlist)
-            for line in nbrlist:
-                if line[i] == matchnbr[most_least][i]:
-                    bufferlist.append(line)
-            nbrlist = bufferlist.copy()
+            matchnbrs = self.__get_most_and_least_common(nbrlist)
+            nbrlist = [line for line in nbrlist if line[bit_idx] == matchnbrs[most_least][bit_idx]]
         return int(nbrlist[0], 2)
 
 
 def main() -> int:
     with open('../Inputfiles/aoc3.txt', 'r') as file:
         mydiag = Diagnostics(file.read().strip('\n'))
-    print("Part 1: ", mydiag.getpowerconsumption())
-    print("Part 2: ", mydiag.getlifesupportrating())
+    print(f"Part 1: {mydiag.getpowerconsumption()}")
+    print(f"Part 2: {mydiag.getlifesupportrating()}")
     return 0
 
 
