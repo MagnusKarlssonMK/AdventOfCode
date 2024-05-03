5017fae65014a123b8831fa5d33f2aa9c04e113f
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-03 23:56:40 +0200

    Some cleanup.

--- 2021/Day_06/aoc6.py
+++ 2021/Day_06/aoc6.py
@@ -6,22 +6,30 @@ to state-0 and add that number to state-6 and also append that number to state-8
 import sys
 
 
-def main() -> int:
-    fish_states = [0 for _ in range(9)]
-    with open('../Inputfiles/aoc6.txt', 'r') as file:
-        for nbr in list(map(int, file.read().strip('\n').split(','))):
-            fish_states[nbr] += 1
+class Fishies:
+    def __init__(self, rawstr: str):
+        nbrs = list(map(int, rawstr.split(',')))
+        self.__states = [0 for _ in range(9)]
+        for nbr in nbrs:
+            self.__states[nbr] += 1
 
-    # 80 days:
-    p1 = 0
-    for n in range(256):
-        state_0 = fish_states.pop(0)
-        fish_states[6] += state_0
-        fish_states.append(state_0)
-        if n == 79:
-            p1 = sum(fish_states)
-    print("Part 1: ", p1)
-    print("Part 2: ", sum(fish_states))
+    def get_answers(self) -> tuple[int, int]:
+        p1 = 0
+        for n in range(256):
+            state_0 = self.__states.pop(0)
+            self.__states[6] += state_0
+            self.__states.append(state_0)
+            if n == 79:
+                p1 = sum(self.__states)
+        return p1, sum(self.__states)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc6.txt', 'r') as file:
+        fishies = Fishies(file.read().strip('\n'))
+    p1, p2 = fishies.get_answers()
+    print(f"Part 1: {p1}")
+    print(f"Part 2: {p2}")
     return 0
 
 
