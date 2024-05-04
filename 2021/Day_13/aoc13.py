bd93ed4f22e4b7249f2a6fce2b6c0f4df904e403
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-04 19:20:02 +0200

    Some cleanup.

--- 2021/Day_13/aoc13.py
+++ 2021/Day_13/aoc13.py
@@ -6,48 +6,50 @@ import sys
 
 
 class Paper:
-    def __init__(self, coordlist: list[tuple[int, int]]):
-        self.coordinates = set()
-        [self.coordinates.add(c) for c in coordlist]
-        self.height = -1
-        self.width = -1
+    def __init__(self, rawstr: str) -> None:
+        c, f = rawstr.split('\n\n')
+        # Flip the coordinates so x,y -> row, col
+        self.__coordinates = set([(int(b), int(a)) for a, b in [coord.split(',') for coord in c.splitlines()]])
+        self.__folding = [(j, int(k)) for j, k in [i.strip('fold along ').split('=') for i in f.splitlines()]]
+        self.__height = -1
+        self.__width = -1
 
-    def fold(self, axis: str, value: int):
+    def __fold(self, axis: str, value: int) -> None:
         if axis == 'x':  # Col
-            coords = [c for c in self.coordinates]
+            coords = [c for c in self.__coordinates]
             for row, col in coords:
                 if col > value:
-                    self.coordinates.remove((row, col))
-                    self.coordinates.add((row, 2 * value - col))
-            self.width = value
+                    self.__coordinates.remove((row, col))
+                    self.__coordinates.add((row, 2 * value - col))
+            self.__width = value
         elif axis == 'y':  # Row
-            coords = [c for c in self.coordinates]
+            coords = [c for c in self.__coordinates]
             for row, col in coords:
                 if row > value:
-                    self.coordinates.remove((row, col))
-                    self.coordinates.add((2 * value - row, col))
-            self.height = value
+                    self.__coordinates.remove((row, col))
+                    self.__coordinates.add((2 * value - row, col))
+            self.__height = value
+
+    def fold_and_get_dots(self) -> int:
+        retval = -1
+        for idx, fold in enumerate(self.__folding):
+            self.__fold(*fold)
+            if idx == 0:
+                retval = len(self.__coordinates)
+        return retval
 
     def __str__(self):
-        grid = [['.' for _ in range(self.width)] for _ in range(self.height)]
-        for r, c in self.coordinates:
+        grid = [['.' for _ in range(self.__width)] for _ in range(self.__height)]
+        for r, c in self.__coordinates:
             grid[r][c] = "#"
         return f"{''.join([line + '\n' for line in [''.join(s) for s in grid]])}"
 
 
 def main() -> int:
     with open('../Inputfiles/aoc13.txt', 'r') as file:
-        c, f = file.read().strip('\n').split('\n\n')
-    # Flip the coordinates so x,y -> row, col
-    coordinates = [(int(b), int(a)) for a, b in [coord.split(',') for coord in c.splitlines()]]
-    folding = [(j, int(k)) for j, k in [i.strip('fold along ').split('=') for i in f.splitlines()]]
-    mypaper = Paper(coordinates)
-    for idx, fold in enumerate(folding):
-        mypaper.fold(*fold)
-        if idx == 0:
-            print("Part 1:", len(mypaper.coordinates))
-    print("Part 2:")
-    print(mypaper)
+        mypaper = Paper(file.read().strip('\n'))
+    print(f"Part 1: {mypaper.fold_and_get_dots()}")
+    print(f"Part 2: \n{mypaper}")
     return 0
 
 
