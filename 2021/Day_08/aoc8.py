250c1d701941d4af5ec3400aada45fea480422ab
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-04 17:57:14 +0200

    Some cleanup.

--- 2021/Day_08/aoc8.py
+++ 2021/Day_08/aoc8.py
@@ -10,53 +10,63 @@ Store the signals in sets to be able to use the '-' operator to find the differe
 """
 import sys
 
-NumSegMap: dict[int: str] = {0: set('abcefg'), 1: set('cf'), 2: set('acdeg'), 3: set('acdfg'), 4: set('bcdf'),
-                             5: set('abdfg'), 6: set('abdefg'), 7: set('acf'), 8: set('abcdefg'), 9: set('abcdfg')}
 
+class Display:
+    NumSegMap: dict[int: str] = {0: set('abcefg'), 1: set('cf'), 2: set('acdeg'), 3: set('acdfg'), 4: set('bcdf'),
+                                 5: set('abdfg'), 6: set('abdefg'), 7: set('acf'), 8: set('abcdefg'), 9: set('abcdfg')}
 
-def setwires(pattern: list[str], output: list[str]) -> int:
-    """Takes the corrupted input pattern and output and returns the actual correct output"""
-    numbers: dict[int: list[set[str]]] = {nbr: [] for nbr in range(10)}
-    mapping: dict[str: str] = {}
-    for p in pattern:  # Store candidate signals for each number based on length
-        for key in NumSegMap:
-            if len(p) == len(NumSegMap[key]):
-                numbers[key].append(set(p))
-    mapping['a'] = numbers[7][0] - numbers[1][0]  # Determine 'a' from 7 and 1
-    [numbers[3].pop(n) for n in reversed(range(len(numbers[3]))) if len(numbers[7][0] - numbers[3][n]) > 0]  # Id 3
-    mapping['b'] = numbers[4][0] - numbers[3][0]  # Determine 'b' from 4 and 3
-    mapping['d'] = numbers[4][0] - numbers[1][0] - mapping['b']
-    [numbers[2].pop(n) for n in reversed(range(len(numbers[2])))
-        if len(mapping['b'] - numbers[2][n]) == 0 or numbers[2][n] == numbers[3][0]]  # Id 2
-    [numbers[5].pop(n) for n in reversed(range(len(numbers[5])))
-        if len(mapping['b'] - numbers[5][n]) > 0 or numbers[5][n] == numbers[3][0]]  # Id 5
-    mapping['c'] = numbers[7][0] - numbers[5][0]  # Determine 'c' from 7 and 5
-    mapping['f'] = numbers[7][0] - numbers[2][0]  # Determine 'c' from 7 and 2
-    mapping['e'] = numbers[2][0] - numbers[3][0]  # Determine 'c' from 2 and 3
-    mapping['g'] = numbers[3][0] - numbers[7][0] - mapping['d']  # Determine 'c' from 2 and 3
+    def __init__(self, rawstr: str):
+        self.__lines = [(row[0].split(), row[1].split()) for row in
+                        [line.split(' | ') for line in rawstr.splitlines()]]
 
-    # Invert the map and translate the output
-    imapping = {''.join(v): k for k, v in mapping.items()}
-    retstr = ''
-    for signal in output:
-        sigset = set()
-        [sigset.add(imapping[c]) for c in signal]
-        for nbr in NumSegMap:
-            if sigset == NumSegMap[nbr]:
-                retstr += str(nbr)
-                break
-    return int(retstr)
+    def get_1478_count(self) -> int:
+        p1_result = 0
+        for _, right in self.__lines:
+            p1_result += sum([1 for word in right if len(word) in (2, 3, 4, 7)])
+        return p1_result
+
+    def get_output_sum(self) -> int:
+        return sum([self.__set_wires(i) for i, _ in enumerate(self.__lines)])
+
+    def __set_wires(self, line_idx: int) -> int:
+        pattern, output = self.__lines[line_idx]
+        """Takes the corrupted input pattern and output and returns the actual correct output"""
+        numbers: dict[int: list[set[str]]] = {nbr: [] for nbr in range(10)}
+        mapping: dict[str: str] = {}
+        for p in pattern:  # Store candidate signals for each number based on length
+            for key in Display.NumSegMap:
+                if len(p) == len(Display.NumSegMap[key]):
+                    numbers[key].append(set(p))
+        mapping['a'] = numbers[7][0] - numbers[1][0]  # Determine 'a' from 7 and 1
+        [numbers[3].pop(n) for n in reversed(range(len(numbers[3]))) if len(numbers[7][0] - numbers[3][n]) > 0]  # Id 3
+        mapping['b'] = numbers[4][0] - numbers[3][0]  # Determine 'b' from 4 and 3
+        mapping['d'] = numbers[4][0] - numbers[1][0] - mapping['b']
+        [numbers[2].pop(n) for n in reversed(range(len(numbers[2])))
+            if len(mapping['b'] - numbers[2][n]) == 0 or numbers[2][n] == numbers[3][0]]  # Id 2
+        [numbers[5].pop(n) for n in reversed(range(len(numbers[5])))
+            if len(mapping['b'] - numbers[5][n]) > 0 or numbers[5][n] == numbers[3][0]]  # Id 5
+        mapping['c'] = numbers[7][0] - numbers[5][0]  # Determine 'c' from 7 and 5
+        mapping['f'] = numbers[7][0] - numbers[2][0]  # Determine 'c' from 7 and 2
+        mapping['e'] = numbers[2][0] - numbers[3][0]  # Determine 'c' from 2 and 3
+        mapping['g'] = numbers[3][0] - numbers[7][0] - mapping['d']  # Determine 'c' from 2 and 3
+
+        # Invert the map and translate the output
+        imapping = {''.join(v): k for k, v in mapping.items()}
+        retstr = ''
+        for signal in output:
+            sigset = {imapping[c] for c in signal}
+            for nbr in Display.NumSegMap:
+                if sigset == Display.NumSegMap[nbr]:
+                    retstr += str(nbr)
+                    break
+        return int(retstr)
 
 
 def main() -> int:
     with open('../Inputfiles/aoc8.txt', 'r') as file:
-        lines = [(row[0].split(), row[1].split()) for row in
-                 [line.split(' | ') for line in file.read().strip('\n').splitlines()]]
-        p1_result = 0
-        for line in lines:
-            p1_result += sum([1 for word in line[1] if len(word) in (2, 3, 4, 7)])
-        print(f"Part 1: {p1_result}")
-        print(f"Part 2: {sum([setwires(row[0], row[1]) for row in lines])}")
+        display = Display(file.read().strip('\n'))
+    print(f"Part 1: {display.get_1478_count()}")
+    print(f"Part 2: {display.get_output_sum()}")
     return 0
 
 
