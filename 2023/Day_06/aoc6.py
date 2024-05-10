1215c3456d1b9991c811714a695cb7523b5d8a3e
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-10 20:08:17 +0200

    Some restructuring.

--- 2023/Day_06/aoc6.py
+++ 2023/Day_06/aoc6.py
@@ -2,7 +2,6 @@
 Uses quadratic formula to calculate the two points where the score intersects the old record. One of the surprising
 challenges was to round them off in the right direction, to also account for the few cases where the solution was
 exactly the same value as the old record.
-As it turned out, perhaps not super useful to create a class for storing the races.
 """
 import sys
 import math
@@ -10,34 +9,38 @@ import re
 
 
 class Race:
-    def __init__(self, t: int, d: int):
+    def __init__(self, t: int, d: int) -> None:
         self.time = t
         self.distance = d
-        self.minvelocity = math.floor((self.time - math.sqrt(self.time**2 - (4 * self.distance))) / 2) + 1
-        self.maxvelocity = math.ceil((self.time + math.sqrt(self.time**2 - (4 * self.distance))) / 2) - 1
-        self.score = 1 + self.maxvelocity - self.minvelocity
 
-    def __str__(self):
-        return (f"Time: {self.time}, Distance: {self.distance}, MinVel: {self.minvelocity},\\"
-                f" MaxVel: {self.maxvelocity}, Score: {self.score}")
+    def get_score(self) -> int:
+        minvelocity = math.floor((self.time - math.sqrt(self.time**2 - (4 * self.distance))) / 2) + 1
+        maxvelocity = math.ceil((self.time + math.sqrt(self.time**2 - (4 * self.distance))) / 2) - 1
+        return 1 + maxvelocity - minvelocity
+
+
+class Competition:
+    def __init__(self, rawstr: str) -> None:
+        t, d = rawstr.splitlines()
+        timelist = list(map(int, re.findall(r"\d+", t)))
+        distancelist = list(map(int, re.findall(r"\d+", d)))
+        self.__races = [Race(timelist[idx], distancelist[idx]) for idx, _ in enumerate(timelist)]
+
+    def get_race_product(self) -> int:
+        return math.prod([race.get_score() for race in self.__races])
+
+    def get_megarace_score(self) -> int:
+        t = int(''.join([str(race.time) for race in self.__races]))
+        d = int(''.join([str(race.distance) for race in self.__races]))
+        megarace = Race(t, d)
+        return megarace.get_score()
 
 
 def main() -> int:
     with open("../Inputfiles/aoc6.txt", "r") as file:
-        timelist = list(map(int, re.findall(r"\d+", file.readline())))
-        distancelist = list(map(int, re.findall(r"\d+", file.readline())))
-
-    # ---- Part 1 ----
-    racelist_p1: list[Race] = [Race(timelist[idx], distancelist[idx]) for idx in range(len(timelist))]
-    result_p1 = math.prod([race.score for race in racelist_p1])
-    print("Part1:", result_p1)
-
-    # ---- Part 2 ----
-    racetime_p2 = int(''.join([str(race.time) for race in racelist_p1]))
-    distance_p2 = int(''.join([str(race.distance) for race in racelist_p1]))
-
-    race_p2 = Race(racetime_p2, distance_p2)
-    print("Part2:", race_p2.score)
+        myrace = Competition(file.read().strip('\n'))
+    print(f"Part 1: {myrace.get_race_product()}")
+    print(f"Part 2: {myrace.get_megarace_score()}")
     return 0
 
 
