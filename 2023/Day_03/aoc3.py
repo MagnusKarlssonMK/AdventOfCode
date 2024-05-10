a0232e3c9d9cb670b13cd0400533161a914e26e9
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-10 14:26:01 +0200

    A bit of restructure and cleanup to move things away from main. Move parts and gears representation to dataclasses to avoid excessive index abuse.

--- 2023/Day_03/aoc3.py
+++ 2023/Day_03/aoc3.py
@@ -1,53 +1,68 @@
+"""
+Store the symbols in a dict with the type stored for each symbol, and then a dict for all parts which stores a list
+of adjacent symbols for each part.
+Then for part 1, simply iterate over the parts and get the sum of the values for the parts that have at least one
+symbol in its adjacent list.
+For part 2, instead iterate over the symbols and find the gears, and then iterate over the parts to see how many parts
+that are adjacent for each gear.
+"""
 import sys
 import re
+from dataclasses import dataclass
 
-Part = tuple[int, int, int, int]  # Row, Col, Len, value
-Gear = tuple[int, int]  # Row, Col
 
-partlist: dict[Part: list[Gear]] = {}
-grid = []
+@dataclass(frozen=True)
+class Part:
+    row: int
+    col: int
+    length: int
+    value: int
+
+
+@dataclass(frozen=True)
+class Symbol:
+    row: int
+    col: int
+
+
+class Schematic:
+    def __init__(self, rawstr: str) -> None:
+        self.__parts: dict[Part: list[Symbol]] = {}
+        self.__symbols: dict[Symbol: str] = {}
+        # Parse input
+        for rowidx, row in enumerate(rawstr.splitlines()):
+            nbrs = re.finditer(r"(\d+)", row)
+            symbs = re.finditer(r"[^.\d]", row)
+            for nbr in nbrs:
+                self.__parts[Part(rowidx, nbr.start(), nbr.end() - nbr.start(), int(row[nbr.start():nbr.end()]))] = []
+            for symb in symbs:
+                self.__symbols[Symbol(rowidx, symb.start())] = row[symb.start()]
+        # Connect symbols to parts
+        for part in self.__parts:
+            for rowidx in range(part.row - 1, part.row + 2):
+                for colidx in range(part.col - 1, part.col + part.length + 1):
+                    if (adj_symb := Symbol(rowidx, colidx)) in self.__symbols:
+                        self.__parts[part].append(adj_symb)
+
+    def get_partnumber_sum(self) -> int:
+        return sum([part.value for part in self.__parts if len(self.__parts[part]) > 0])
+
+    def get_gearratio_sum(self) -> int:
+        retval = 0
+        for symbol, symbtype in self.__symbols.items():
+            if symbtype == "*":
+                adj_parts = [part.value for part in self.__parts if symbol in self.__parts[part]]
+                if len(adj_parts) == 2:
+                    retval += adj_parts[0] * adj_parts[1]
+        return retval
 
 
 def main() -> int:
     with open('../Inputfiles/aoc3.txt', 'r') as file:
-        [grid.append(line.strip("\n")) for line in file.readlines()]
+        myschematic = Schematic(file.read().strip('\n'))
 
-    symbollist = []
-
-    for rowidx, row in enumerate(grid):
-        nbrs = re.finditer(r"(\d+)", row)
-        symbs = re.finditer(r"[^.\d]", row)
-        for nbr in nbrs:
-            partlist[rowidx, nbr.start(), nbr.end() - nbr.start(), int(row[nbr.start():nbr.end()])] = []
-        for symb in symbs:
-            symbollist.append((rowidx, symb.start()))
-
-    result_p1 = 0
-
-    for key in list(partlist.keys()):
-        for rowidx in range(key[0] - 1, key[0] + 2):
-            for colidx in range(key[1] - 1, key[1] + key[2] + 1):
-                if (rowidx, colidx) in symbollist:
-                    partlist[key].append((rowidx, colidx))
-        if len(partlist[key]) > 0:
-            result_p1 += key[3]
-
-    print("Part1: ", result_p1)
-
-    result_p2 = 0
-
-    for sym in symbollist:
-        if grid[sym[0]][sym[1]] == "*":
-            keylist = []
-            [keylist.append(key) for key in list(partlist.keys()) if sym[0] - 1 <= key[0] <= sym[0] + 1]
-            adjacent = []
-            for part in keylist:
-                if sym in partlist[part]:
-                    adjacent.append(part)
-            if len(adjacent) == 2:
-                result_p2 += adjacent[0][3] * adjacent[1][3]
-
-    print("Part2: ", result_p2)
+    print(f"Part 1 redux: {myschematic.get_partnumber_sum()}")
+    print(f"Part 2 redux: {myschematic.get_gearratio_sum()}")
     return 0
 
 
