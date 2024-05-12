bc08b642e7277708c179735aaadff9fd3741f2c7
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-12 21:53:27 +0200

    Some cleanup. Make code slightly easier to read with some simple dataclasses.

--- 2023/Day_17/aoc17.py
+++ 2023/Day_17/aoc17.py
@@ -1,50 +1,81 @@
+"""
+Sort of a Dijkstra solution but with a bit more complicated state, since it also needs to track direction.
+Certainly not fast, but gets the job done.
+"""
 import sys
 from heapq import heappop, heappush
+from dataclasses import dataclass
 
-RowCol = tuple[int, int]
-Directions = {'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)}
+
+@dataclass(frozen=True)
+class Coord:
+    row: int
+    col: int
+
+    def rotate_ccw(self) -> "Coord":
+        return Coord(-self.col, self.row)
+
+    def rotate_cw(self) -> "Coord":
+        return Coord(self.col, -self.row)
+
+    def __add__(self, other: "Coord") -> "Coord":
+        return Coord(self.row + other.row, self.col + other.col)
+
+
+@dataclass(frozen=True)
+class State:
+    pos: Coord
+    direction: Coord
+    steps: int
+
+    def __lt__(self, other: "State"):
+        return self.steps < other.steps
 
 
 class CityBoard:
-    def __init__(self, rawstr: str):
-        self.grid = [[int(c) for c in line] for line in rawstr.splitlines()]
-        self.width = len(self.grid[0])
-        self.height = len(self.grid)
-        self.minsteps = 0
-        self.maxsteps = 3
+    def __init__(self, rawstr: str) -> None:
+        self.__grid = [[int(c) for c in line] for line in rawstr.splitlines()]
+        self.__width = len(self.__grid[0])
+        self.__height = len(self.__grid)
 
-    def findshortestpath(self) -> int:
-        start: RowCol = (0, 0)
-        target: RowCol = (self.height - 1, self.width - 1)
+    def __get_value(self, point: Coord) -> int:
+        if (0 <= point.row < self.__height) and (0 <= point.col < self.__width):
+            return self.__grid[point.row][point.col]
+        return -1
+
+    def get_shortestpath(self, minsteps: int = 0, maxsteps: int = 3) -> int:
+        state = State(Coord(0, 0), Coord(0, 1), 0)
+        target = Coord(self.__height - 1, self.__width - 1)
         visited = {}
         queue = []
-        heappush(queue, (0, start, (0, 1), 0))
+        heappush(queue, (0, state))
         while queue:
-            heat, pos, drn, stepcount = heappop(queue)
-            if pos == target and stepcount >= self.minsteps:
+            heat, state = heappop(queue)
+            if state.pos == target and state.steps >= minsteps:
                 return heat
-            neighbors = []
-            if stepcount >= self.minsteps:
-                neighbors.append(((pos[0] - drn[1], pos[1] + drn[0]), (-drn[1], drn[0]), 1))
-                neighbors.append(((pos[0] + drn[1], pos[1] - drn[0]), (drn[1], -drn[0]), 1))
-            if stepcount < self.maxsteps:
-                neighbors.append(((pos[0] + drn[0], pos[1] + drn[1]), drn, stepcount + 1))
-            for n in neighbors:
-                if 0 <= n[0][0] < self.height and 0 <= n[0][1] < self.width:
-                    new_heat = heat + self.grid[n[0][0]][n[0][1]]
-                    if n not in visited or new_heat < visited[n]:
-                        visited[n] = new_heat
-                        heappush(queue, (new_heat, *n))
+            neighborstates: list[State] = []
+            if state.steps >= minsteps:
+                ccw = state.direction.rotate_ccw()
+                neighborstates.append(State(state.pos + ccw, ccw, 1))
+                cw = state.direction.rotate_cw()
+                neighborstates.append(State(state.pos + cw, cw, 1))
+            if state.steps < maxsteps:
+                neighborstates.append(State(state.pos + state.direction, state.direction, state.steps + 1))
+            for ns in neighborstates:
+                if (newheat := self.__get_value(ns.pos)) < 0:
+                    continue
+                newheat += heat
+                if ns not in visited or newheat < visited[ns]:
+                    visited[ns] = newheat
+                    heappush(queue, (newheat, ns))
         return -1
 
 
 def main() -> int:
-    with open('../Inputfiles/aoc17.txt') as file:
-        mygrid = CityBoard(file.read().strip('\n'))
-    print("Part 1:", mygrid.findshortestpath())
-    mygrid.minsteps = 4
-    mygrid.maxsteps = 10
-    print("Part 2:", mygrid.findshortestpath())
+    with open('../Inputfiles/aoc17.txt', 'r') as file:
+        mycity = CityBoard(file.read().strip('\n'))
+    print(f"Part 1: {mycity.get_shortestpath()}")
+    print(f"Part 2: {mycity.get_shortestpath(4, 10)}")
     return 0
 
 
