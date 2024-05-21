f774603924a868f4c437b3b58b83783518209b2b
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-21 23:51:31 +0200

    Encapsulate solution in a class.

--- 2020/Day_01/aoc1.py
+++ 2020/Day_01/aoc1.py
@@ -4,30 +4,34 @@ Simply loop through the list of numbers with 2/3 indices to check all combinatio
 import sys
 
 
-def find2020pair(numbers: list[int]) -> int:
-    for i in range(len(numbers) - 1):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == 2020:
-                return numbers[i] * numbers[j]
-    return -1
+class ExpenseReport:
+    __TARGET = 2020
 
+    def __init__(self, rawstr: str) -> None:
+        self.__numbers = [int(line) for line in rawstr.splitlines()]
 
-def find2020triad(numbers: list[int]) -> int:
-    for i in range(len(numbers) - 2):
-        for j in range(i + 1, len(numbers) - 1):
-            if (v := numbers[i] + numbers[j]) > 2020:
-                continue
-            for k in range(j + 1, len(numbers)):
-                if v + numbers[k] == 2020:
-                    return numbers[i] * numbers[j] * numbers[k]
-    return -1
+    def get_2020_pair(self) -> int:
+        for i in range(len(self.__numbers) - 1):
+            for j in range(i + 1, len(self.__numbers)):
+                if self.__numbers[i] + self.__numbers[j] == ExpenseReport.__TARGET:
+                    return self.__numbers[i] * self.__numbers[j]
+        return -1
+
+    def get_2020_triplet(self) -> int:
+        for i in range(len(self.__numbers) - 2):
+            for j in range(i + 1, len(self.__numbers) - 1):
+                if (pairsum := self.__numbers[i] + self.__numbers[j]) < ExpenseReport.__TARGET:
+                    for k in range(j + 1, len(self.__numbers)):
+                        if pairsum + self.__numbers[k] == ExpenseReport.__TARGET:
+                            return self.__numbers[i] * self.__numbers[j] * self.__numbers[k]
+        return -1
 
 
 def main() -> int:
     with open('../Inputfiles/aoc1.txt', 'r') as file:
-        numbers = list(map(int, file.read().strip('\n').splitlines()))
-    print("Part 1:", find2020pair(numbers))
-    print("Part 2:", find2020triad(numbers))
+        report = ExpenseReport(file.read().strip('\n'))
+    print(f"Part 1: {report.get_2020_pair()}")
+    print(f"Part 2: {report.get_2020_triplet()}")
     return 0
 
 
