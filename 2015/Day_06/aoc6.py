bb47a20ef1828942154f5d50291eba5fc3e23de7
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-26 19:23:52 +0200

    First version.

---
+++ 2015/Day_06/aoc6.py
@@ -0,0 +1,73 @@
+"""
+A bit of numpy exercise to make things run decently fast.
+"""
+import sys
+from dataclasses import dataclass
+from enum import Enum
+import re
+import numpy as np
+
+
+class Action(Enum):
+    TOGGLE = 0
+    TURN_ON = 1
+    TURN_OFF = 2
+
+
+@dataclass
+class Instruction:
+    action: Action
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+
+
+class LightGrid:
+    def __init__(self, rawstr: str) -> None:
+        self.__instructions = []
+        for line in rawstr.splitlines():
+            nbrs = list(map(int, re.findall(r'\d+', line)))
+            words = line.split()
+            action = Action.TOGGLE
+            if words[0] != 'toggle':
+                action = Action.TURN_ON if words[1] == 'on' else Action.TURN_OFF
+            self.__instructions.append(Instruction(action, *nbrs))
+
+    def get_lights_count(self) -> int:
+        grid = np.zeros((1000, 1000))
+        for instr in self.__instructions:
+            match instr.action:
+                case Action.TURN_ON:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] = 1
+                case Action.TURN_OFF:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] = 0
+                case Action.TOGGLE:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] += 1
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] %= 2
+        return int(grid.sum())
+
+    def get_correct_lights_count(self) -> int:
+        grid = np.zeros((1000, 1000))
+        for instr in self.__instructions:
+            match instr.action:
+                case Action.TURN_ON:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] += 1
+                case Action.TURN_OFF:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] -= 1
+                    grid.clip(min=0, out=grid)
+                case Action.TOGGLE:
+                    grid[instr.x1: instr.x2 + 1, instr.y1: instr.y2 + 1] += 2
+        return int(grid.sum())
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc6.txt', 'r') as file:
+        grid = LightGrid(file.read().strip('\n'))
+    print(f"Part 1: {grid.get_lights_count()}")
+    print(f"Part 2: {grid.get_correct_lights_count()}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
