1d9fd2b701830cc86c60bb6d41584f34d75e61ce
BigMange <155587399+BigMange@users.noreply.github.com>
2024-05-31 16:45:44 +0200

    First version of 2015 day 10.

---
+++ 2015/Day_10/aoc10.py
@@ -0,0 +1,43 @@
+"""
+Pretty much just walk through the string and generate the new number, so basically brute-force. Part 1 is decently
+fast but part 2 takes a couple of seconds. Might need to investigate further in the future if there are more clever
+ways to approach this, such as grouping by repeating patterns of sub-numbers or similar.
+"""
+import sys
+
+
+class LookAndSay:
+    def __init__(self, rawstr: str) -> None:
+        self.__startnbrs = rawstr
+
+    def get_generated_length(self, rounds: int) -> int:
+        sequence = self.__startnbrs
+        for _ in range(rounds):
+            new_seq = ''
+            count = 0
+            currentchar = ''
+            for c in sequence:
+                if c == currentchar:
+                    count += 1
+                else:
+                    if count > 0:
+                        new_seq += str(count) + currentchar
+                    count = 1
+                    currentchar = c
+            if count > 0:
+                new_seq += str(count) + currentchar
+            sequence = new_seq
+        # print(sequence)
+        return len(sequence)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc10.txt', 'r') as file:
+        game = LookAndSay(file.read().strip('\n'))
+    print(f"Part 1: {game.get_generated_length(40)}")
+    print(f"Part 2: {game.get_generated_length(50)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
