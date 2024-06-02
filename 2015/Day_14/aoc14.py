e55d2876e075d7c1e83316aa90dc28db9a64418c
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-02 18:06:09 +0200

    First version of 2015 day 14.

---
+++ 2015/Day_14/aoc14.py
@@ -0,0 +1,63 @@
+"""
+Surprisingly straightforward - pretty much just parse the reindeers into a class that gives a method to calculate
+distance based on time.
+For part 2 we simply need to simulate the race second-for-second and award points along the way. The only minor thing
+to keep in mind is that the time loop needs to start at 1 to avoid awarding points on the starting line, and to run
+until 2503+1 to compensate for python's range not including the last value.
+"""
+import sys
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Reindeer:
+    speed: int
+    time: int
+    rest: int
+
+    def get_distance(self, time: int) -> int:
+        cycle_time = (self.time + self.rest)
+        cycles = time // cycle_time
+        remainder = time % cycle_time
+        total = self.speed * ((cycles * self.time) + min(remainder, self.time))
+        return total
+
+
+class Olympics:
+    def __init__(self, rawstr: str) -> None:
+        self.__reindeers: dict[str: Reindeer] = {}
+        for line in rawstr.splitlines():
+            nbrs = list(map(int, re.findall(r"\d+", line)))
+            name = line.split()[0]
+            self.__reindeers[name] = Reindeer(nbrs[0], nbrs[1], nbrs[2])
+
+    def get_winner_distance(self, new_scoring: bool = False, time: int = 2503) -> int:
+        if not new_scoring:
+            return max([self.__reindeers[r].get_distance(time) for r in self.__reindeers])
+        scoretable: dict[str: int] = {r: 0 for r in self.__reindeers}
+        for seconds in range(1, time + 1):  # Start on 1, so we don't award points at the starting line!
+            leader_dist = 0
+            leaders = []
+            for r in self.__reindeers:
+                newdist = self.__reindeers[r].get_distance(seconds)
+                if newdist > leader_dist:
+                    leaders = [r]
+                    leader_dist = newdist
+                elif newdist == leader_dist:
+                    leaders.append(r)
+            for lead in leaders:
+                scoretable[lead] += 1
+        return max(scoretable.values())
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc14.txt', 'r') as file:
+        olympics = Olympics(file.read().strip('\n'))
+    print(f"Part 1: {olympics.get_winner_distance()}")
+    print(f"Part 2: {olympics.get_winner_distance(True)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
