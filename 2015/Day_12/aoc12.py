c2197a2e05be7bcca4a28dda5c7b0d3fe55b1d26
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-02 11:53:42 +0200

    First version of 2015 day 12.

---
+++ 2015/Day_12/aoc12.py
@@ -0,0 +1,38 @@
+"""
+Uses the json module to load the data and then traverses it recursevly to count the content.
+"""
+import sys
+import json
+
+
+class JSON:
+    def __init__(self, rawstr: str) -> None:
+        self.__jsonstr = rawstr
+        self.__json = json.loads(rawstr)
+
+    def get_number_sum(self, ignored: str = None) -> int:
+        return self.__json_object_count(self.__json, ignored)
+
+    def __json_object_count(self, json_input, ignored: str = None) -> int:
+        if isinstance(json_input, dict):
+            if ignored and ignored in json_input.values():
+                return 0
+            else:
+                return sum([self.__json_object_count(json_input[j], ignored) for j in json_input])
+        if isinstance(json_input, list):
+            return sum([self.__json_object_count(j, ignored) for j in json_input])
+        if isinstance(json_input, int):
+            return json_input
+        return 0  # strings have no value
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc12.txt', 'r') as file:
+        json_data = JSON(file.read().strip('\n'))
+    print(f"Part 1: {json_data.get_number_sum()}")
+    print(f"Part 2: {json_data.get_number_sum('red')}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
