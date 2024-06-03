eec36d48395a3083373c01460294f17beef6f08b
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-04 01:01:44 +0200

    First version of 2015 day 18.

---
+++ 2015/Day_18/aoc18.py
@@ -0,0 +1,81 @@
+"""
+Pretty basic game of life. Not a very efficient solution, but gets the job done in a couple of seconds.
+Would probably be much improved just by using numpy instead.
+"""
+import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Coord:
+    row: int
+    col: int
+
+    def __add__(self, other: 'Coord') -> 'Coord':
+        return Coord(self.row + other.row, self.col + other.col)
+
+
+class Grid:
+    __NEIGHBORS = [Coord(-1, -1), Coord(-1, 0), Coord(-1, 1), Coord(0, -1),
+                   Coord(0, 1), Coord(1, -1), Coord(1, 0), Coord(1, 1)]
+
+    def __init__(self, rawstr: str) -> None:
+        self.__startgrid = []
+        self.__currentgrid = []
+        for line in rawstr.splitlines():
+            self.__startgrid.append([1 if c == '#' else 0 for c in line])
+            self.__currentgrid.append(list(self.__startgrid[-1]))
+        self.__height = len(self.__startgrid)
+        self.__width = len(self.__startgrid[0])
+        self.__corners = [(0, 0), (0, self.__width - 1), (self.__height - 1, 0), (self.__height - 1, self.__width - 1)]
+
+    def __get_neighbor_sum(self, c: Coord) -> int:
+        n_sum = 0
+        for n in Grid.__NEIGHBORS:
+            point = c + n
+            if 0 <= point.row < self.__height and 0 <= point.col < self.__width:
+                n_sum += self.__currentgrid[point.row][point.col]
+        return n_sum
+
+    def __process_grid(self, corners_locked: bool) -> None:
+        changelist = []
+        for r, row in enumerate(self.__currentgrid):
+            for c, val in enumerate(row):
+                n_val = self.__get_neighbor_sum(Coord(r, c))
+                if val == 1:
+                    if n_val < 2 or n_val > 3:
+                        changelist.append((r, c))
+                else:
+                    if n_val == 3:
+                        changelist.append((r, c))
+        for r, c in changelist:
+            if not corners_locked or (r, c) not in self.__corners:
+                self.__currentgrid[r][c] = (self.__currentgrid[r][c] + 1) % 2
+
+    def get_lights_after_steps(self, corners_locked: bool = False, nbrsteps: int = 100) -> int:
+        if corners_locked:
+            for (r, c) in self.__corners:
+                self.__currentgrid[r][c] = 1
+        for _ in range(nbrsteps):
+            self.__process_grid(corners_locked)
+        retval = sum([sum(r) for r in self.__currentgrid])
+        # reset current
+        self.__currentgrid = []
+        for row in self.__startgrid:
+            self.__currentgrid.append(list(row))
+        return retval
+
+    def __repr__(self):
+        return f"{''.join([''.join(map(str, r))+'\n' for r in self.__currentgrid])}"
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc18.txt', 'r') as file:
+        grid = Grid(file.read().strip('\n'))
+    print(f"Part 1: {grid.get_lights_after_steps()}")
+    print(f"Part 2: {grid.get_lights_after_steps(True)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
