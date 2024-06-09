a40daa6da294c65b3d9909267129dba1f484e936
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-09 15:58:53 +0200

    First version of 2016 day 5.

---
+++ 2016/Day_05/aoc5.py
@@ -0,0 +1,48 @@
+"""
+Iterate with an increasing index value and use hashlib to calculate the md5 checksums to generate the passwords.
+Takes a lot of iterations, i.e. doorbreaking is NOT fast.
+"""
+import sys
+import hashlib
+
+
+class Door:
+    __PASSWORD_LEN = 8
+
+    def __init__(self, rawstr: str) -> None:
+        self.__door_id = rawstr
+
+    def get_password(self) -> str:
+        pwd = ''
+        index = 0
+        while len(pwd) < Door.__PASSWORD_LEN:
+            hashed = hashlib.md5((self.__door_id + str(index)).encode()).hexdigest()
+            if hashed.startswith('00000'):
+                pwd += hashed[5]
+            index += 1
+        return pwd
+
+    def get_advanced_password(self) -> str:
+        pwd = ['_' for _ in range(Door.__PASSWORD_LEN)]
+        index = 0
+        while '_' in pwd:
+            hashed = hashlib.md5((self.__door_id + str(index)).encode()).hexdigest()
+            if hashed.startswith('00000') and hashed[5].isdigit():
+                pos = int(hashed[5])
+                if pos < Door.__PASSWORD_LEN:
+                    if pwd[int(pos)] == '_':
+                        pwd[int(pos)] = hashed[6]
+            index += 1
+        return ''.join(pwd)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc5.txt', 'r') as file:
+        door = Door(file.read().strip('\n'))
+    print(f"Part 1: {door.get_password()}")
+    print(f"Part 2: {door.get_advanced_password()}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
