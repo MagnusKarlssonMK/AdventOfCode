cf7643a4359d8fb6ce54d2bbd5d1166a0a0e4679
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-11 23:18:35 +0200

    First version of 2016 day 9.

---
+++ 2016/Day_09/aoc9.py
@@ -0,0 +1,40 @@
+"""
+Recursive solution with regex, checking if there is a repeat marker in the first character in the current string. If
+so, return the length of the repeated segment plus the remaining length recursively, otherwiste step forward one step
+and try again.
+For Part 2, instead of just calculating the length, use a recursive call again instead.
+"""
+import sys
+import re
+
+
+def get_decompressed_len(compressed: str, version2: bool = False) -> int:
+    def len_func(segment: str):
+        if version2:
+            return get_decompressed_len(segment, version2)
+        else:
+            return len(segment)
+
+    if not compressed:
+        return 0
+    if match := re.match(r"\((\d+)x(\d+)\)", compressed):
+        count, repeat = list(map(int, match.groups()))
+        multiply_start = match.end()
+        multiply_end = multiply_start + count
+        return ((len_func(compressed[multiply_start: multiply_end]) * repeat) +
+                get_decompressed_len(compressed[multiply_end:], version2))
+    else:
+        # No match at start of string, move one step forward and try again
+        return 1 + get_decompressed_len(compressed[1:], version2)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc9.txt', 'r') as file:
+        decompr = file.read().strip('\n')
+    print(f"Part 1: {get_decompressed_len(decompr)}")
+    print(f"Part 2: {get_decompressed_len(decompr, True)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
