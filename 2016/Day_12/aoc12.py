86a28a6f4c8091881cc2126d10e32d4b32bc5cff
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-14 22:22:00 +0200

    First version of 2016 day 12.

---
+++ 2016/Day_12/aoc12.py
@@ -0,0 +1,54 @@
+"""
+Straightforward, simply store the program in a computer class, and then run the program according to the definitions
+of the instructions. For Part 2, simply change the initial value for register C to 1 instead of 0.
+"""
+import sys
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Instruction:
+    instr: str
+    arg1: str
+    arg2: str = None
+
+
+class Computer:
+    def __init__(self, rawstr: str) -> None:
+        self.__program = [Instruction(*line.split()) for line in rawstr.splitlines()]
+
+    def get_a_register(self, initialize_c: bool = False) -> int:
+        sp = 0
+        regs = {reg: 0 for reg in ['a', 'b', 'c', 'd']}
+        if initialize_c:
+            regs['c'] = 1
+
+        while 0 <= sp < len(self.__program):
+            match self.__program[sp].instr:
+                case 'cpy':
+                    if self.__program[sp].arg1.isdigit():
+                        regs[self.__program[sp].arg2] = int(self.__program[sp].arg1)
+                    else:
+                        regs[self.__program[sp].arg2] = regs[self.__program[sp].arg1]
+                case 'inc':
+                    regs[self.__program[sp].arg1] += 1
+                case 'dec':
+                    regs[self.__program[sp].arg1] -= 1
+                case 'jnz':
+                    if self.__program[sp].arg1.isdigit() or regs[self.__program[sp].arg1] != 0:
+                        sp += int(self.__program[sp].arg2)
+                        continue
+            sp += 1
+        return regs['a']
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc12.txt', 'r') as file:
+        computer = Computer(file.read().strip('\n'))
+    print(f"Part 1: {computer.get_a_register()}")
+    print(f"Part 2: {computer.get_a_register(True)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
