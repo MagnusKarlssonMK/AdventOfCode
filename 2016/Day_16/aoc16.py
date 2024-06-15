499d08352105d46c242eae7e2ee7d75e4f51d73c
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-15 21:33:20 +0200

    First version of 2016 day 16.

---
+++ 2016/Day_16/aoc16.py
@@ -0,0 +1,46 @@
+"""
+Basically a brute-force solution, gets part 2 done in a second or so.
+There are some fancier solutions out there, I might return to check those out some rainy day...
+"""
+import sys
+
+
+def dragon_curve(a: str) -> str:
+    b = ''.join(['1' if c == '0' else '0' for c in reversed(a)])
+    return a + '0' + b
+
+
+def checksum(a: str) -> str:
+    if len(a) % 2 != 0:
+        return a
+    else:
+        b = ''
+        for i in range(0, len(a), 2):
+            if a[i] == a[i + 1]:
+                b += '1'
+            else:
+                b += '0'
+        return checksum(b)
+
+
+class Disk:
+    def __init__(self, rawstr: str) -> None:
+        self.__startdata = rawstr
+
+    def get_checksum(self, disksize: int) -> str:
+        data = self.__startdata
+        while len(data) < disksize:
+            data = dragon_curve(data)
+        return checksum(data[0: disksize])
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc16.txt', 'r') as file:
+        disk = Disk(file.read().strip('\n'))
+    print(f"Part 1: {disk.get_checksum(272)}")
+    print(f"Part 2: {disk.get_checksum(35651584)}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
