f86bed6e8b5fa9f3dcd4a9a8444640ed28613e2d
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-21 18:14:58 +0200

    First version of 2017 day 6.

---
+++ 2017/Day_06/aoc6.py
@@ -0,0 +1,42 @@
+"""
+Part 1: Store the memory banks in a class, and rebalance according to the description. Store the current configuration
+in a set after every rebalancing and check if we have a repeated value.
+Part 2: We saved the state of the memory banks after part 1, so simply run the checker again to get the answer for the
+next cycle.
+"""
+import sys
+
+
+class Memory:
+    def __init__(self, rawstr: str) -> None:
+        self.__banks = list(map(int, rawstr.split()))
+
+    def __rebalance(self) -> None:
+        val = max(self.__banks)
+        i = self.__banks.index(val)
+        self.__banks[i] = 0
+        while val > 0:
+            i = (i + 1) % len(self.__banks)
+            self.__banks[i] += 1
+            val -= 1
+
+    def get_cycles_count(self) -> int:
+        seen_configs = {tuple(self.__banks)}
+        while True:
+            self.__rebalance()
+            next_cfg = tuple(self.__banks)
+            if next_cfg in seen_configs:
+                return len(seen_configs)
+            seen_configs.add(next_cfg)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc6.txt', 'r') as file:
+        memory = Memory(file.read().strip('\n'))
+    print(f"Part 1: {memory.get_cycles_count()}")
+    print(f"Part 2: {memory.get_cycles_count()}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
