bf0d3949ade7ea559190d0d9d6127317735663ed
BigMange <155587399+BigMange@users.noreply.github.com>
2024-06-22 15:51:20 +0200

    First version of 2017 day 12.

---
+++ 2017/Day_12/aoc12.py
@@ -0,0 +1,50 @@
+"""
+Although the description sort of teases with 'travelling salesman', this actually only comes down to a number of
+simple BFS's through the map to find out which nodes are connected.
+"""
+import sys
+
+
+class Village:
+    def __init__(self, rawstr: str) -> None:
+        self.__programs: dict[int: set[int]] = {}
+        for line in rawstr.splitlines():
+            left, right = line.split(' <-> ')
+            self.__programs[int(left)] = set(map(int, right.split(', ')))
+        self.__unsorted = [i for i in self.__programs]
+        self.__sorted = []
+
+    def __process_group_containing(self, nbr: int) -> None:
+        seen = set()
+        queue = [nbr]
+        while queue:
+            current = queue.pop(0)
+            if current in seen:
+                continue
+            seen.add(current)
+            for n in self.__programs[current]:
+                queue.append(n)
+        self.__sorted.append(seen)
+        for s in seen:
+            self.__unsorted.remove(s)
+
+    def get_group_0_size(self) -> int:
+        self.__process_group_containing(0)
+        return len(self.__sorted[0])
+
+    def get_groups_count(self) -> int:
+        while self.__unsorted:
+            self.__process_group_containing(self.__unsorted[0])
+        return len(self.__sorted)
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc12.txt', 'r') as file:
+        village = Village(file.read().strip('\n'))
+    print(f"Part 1: {village.get_group_0_size()}")
+    print(f"Part 2: {village.get_groups_count()}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
