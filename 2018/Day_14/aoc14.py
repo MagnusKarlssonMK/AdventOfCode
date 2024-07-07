64b69084515c9bafd0985830e29ce40fe630352b
BigMange <155587399+BigMange@users.noreply.github.com>
2024-07-07 18:42:56 +0200

    First version of 2018 day 14.

---
+++ 2018/Day_14/aoc14.py
@@ -0,0 +1,63 @@
+"""
+Brute force, kinda slow part 2. Not sure if there is some kind of repeating pattern in the generation that could be
+exploited, couldn't really find any after some quick experimentation.
+"""
+import sys
+
+
+class RecipeGenerator:
+    __START_POSITIONS = (0, 1)
+    __START_RECIPES = (3, 7)
+
+    def __init__(self, nbr: int) -> None:
+        self.__inputnbr = nbr
+        self.__scoreboard = list(RecipeGenerator.__START_RECIPES)
+        self.__elfs = list(RecipeGenerator.__START_POSITIONS)
+
+    def __reset(self) -> None:
+        self.__scoreboard = list(RecipeGenerator.__START_RECIPES)
+        self.__elfs = list(RecipeGenerator.__START_POSITIONS)
+
+    def __make_recipes(self) -> int:
+        newcount = 1
+        current_sum = sum([self.__scoreboard[e] for e in self.__elfs])
+        if current_sum > 9:
+            newcount = 2  # Return the number of new recipes, so we can avoid making unnecessary comparisons in part 2
+            self.__scoreboard.append(1)
+        self.__scoreboard.append(current_sum % 10)
+        self.__elfs = [(e + self.__scoreboard[e] + 1) % len(self.__scoreboard) for e in self.__elfs]
+        return newcount
+
+    def get_scores(self) -> int:
+        while len(self.__scoreboard) < self.__inputnbr + 10:
+            self.__make_recipes()
+        result = int(''.join([str(i) for i in self.__scoreboard[self.__inputnbr: self.__inputnbr + 10]]))
+        self.__reset()
+        return result
+
+    def get_scores_left_side(self) -> int:
+        nbrlist = [int(c) for c in str(self.__inputnbr)]
+        nbrlistlen = len(nbrlist)
+        while True:
+            if self.__make_recipes() > 1:
+                # If two new recipes were added, we need to check also -1 from the end
+                if self.__scoreboard[-nbrlistlen - 1: -1] == nbrlist:
+                    result = len(self.__scoreboard) - nbrlistlen - 1
+                    break
+            if self.__scoreboard[-nbrlistlen:] == nbrlist:
+                result = len(self.__scoreboard) - nbrlistlen
+                break
+        self.__reset()
+        return result
+
+
+def main() -> int:
+    with open('../Inputfiles/aoc14.txt', 'r') as file:
+        generator = RecipeGenerator(int(file.read().strip('\n')))
+    print(f"Part 1: {generator.get_scores()}")
+    print(f"Part 2: {generator.get_scores_left_side()}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
