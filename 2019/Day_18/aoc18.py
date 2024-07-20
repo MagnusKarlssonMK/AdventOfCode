70c2948a0987952d9eeb2368356ea61ae7e919b6
BigMange <155587399+BigMange@users.noreply.github.com>
2024-07-20 16:35:36 +0200

    2019-day-18: Refactor part 1 to a much faster solution. Part 2 done.

--- 2019/Day_18/aoc18.py
+++ 2019/Day_18/aoc18.py
@@ -1,9 +1,22 @@
 """
+- Step 1: Parse the input grid into a coordinate dictionary of what possible neighbor points are, and where keys and
+doors are. Also treat the start point as a key, as we will need that in the next step.
+- Step 2: Use the coordinate map to build a map of how the key locations (including the starting point) are connected;
+number of steps and doors to pass. Note that there can in theory exist multiple paths between any two keys, with
+different key requirements.
+- Step 3: Use the key map to perform a Dijkstra search to find the least nbr of steps required to collect all keys from
+the starting point. This will give the answer to Part 1.
+- Step 4: Update the grid according to the description, and then pretty much repeat previous steps with the amended map,
+with the key difference that there are now 4 start points, and the state space for step 3 contains 4 moving parts
+instead of just one. This will give the answer to Part 2.
 
+Note: Quite a bit of duplicated code between Part1 and Part2 functions, can probably be cleaned up a bit.
 """
 import sys
 from dataclasses import dataclass
 from enum import Enum
+from heapq import heappop, heappush
+from copy import deepcopy
 
 
 @dataclass(frozen=True)
@@ -31,8 +44,8 @@ class Tile:
 class Vault:
     def __init__(self, rawstr: str) -> None:
         self.__tiles: dict[Point: Tile] = {}
-        self.__start: Point = Point(-1, -1)
-        self.__nbr_keys = 0
+        self.__keys: dict[str: Point] = {}
+        startpoint: Point = Point(-1, -1)
         for y, line in enumerate(rawstr.splitlines()):
             for x, c in enumerate(line):
                 if c == "#":
@@ -40,44 +53,127 @@ class Vault:
                 elif c == ".":
                     self.__tiles[Point(x, y)] = Tile(TileType.OPEN, '', set())
                 elif c == "@":
-                    self.__tiles[Point(x, y)] = Tile(TileType.OPEN, '', set())
-                    self.__start = Point(x, y)
+                    self.__tiles[Point(x, y)] = Tile(TileType.KEY, c, set())
+                    startpoint = Point(x, y)
+                    self.__keys[c] = Point(x, y)
                 elif c.islower():
                     self.__tiles[Point(x, y)] = Tile(TileType.KEY, c, set())
-                    self.__nbr_keys += 1
+                    self.__keys[c] = Point(x, y)
                 elif c.isupper():
                     self.__tiles[Point(x, y)] = Tile(TileType.DOOR, c, set())
+
         for point in self.__tiles:
             for n in (Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)):
                 if (newpoint := point + n) in self.__tiles:
                     self.__tiles[point].neighbors.add(newpoint)
 
-    def get_shortest_key_steps(self) -> int:
-        seen: set[tuple[Point, tuple[str]]] = set()
-        queue = [(self.__start, '', Point(-1, -1), 0)]
-        while queue:
-            current, keys, previous, steps = queue.pop(0)
-            keyfound = False
-            if self.__tiles[current].type == TileType.KEY and self.__tiles[current].value not in keys:
-                keys += self.__tiles[current].value
-                if len(keys) == self.__nbr_keys:
-                    return steps
-                keyfound = True
-                keys = ''.join(sorted(keys))
-            if (current, keys) in seen:
+        # Create an alternative updated map around the starting position for part 2
+        self.__four_tiles = deepcopy(self.__tiles)
+        self.__four_keys = deepcopy(self.__keys)
+        blocked = (startpoint, Point(startpoint.x, startpoint.y - 1), Point(startpoint.x, startpoint.y + 1),
+                   Point(startpoint.x - 1, startpoint.y), Point(startpoint.x + 1, startpoint.y))
+        for b in blocked:
+            self.__four_tiles.pop(b)
+        for y in range(startpoint.y - 2, startpoint.y + 3):
+            for x in range(startpoint.x - 2, startpoint.x + 3):
+                if (p := Point(x, y)) in self.__four_tiles:
+                    for b in blocked:
+                        if b in self.__four_tiles[p].neighbors:
+                            self.__four_tiles[p].neighbors.remove(b)
+        self.__four_keys['@'] = Point(startpoint.x - 1, startpoint.y - 1)
+        self.__four_tiles[Point(startpoint.x - 1, startpoint.y - 1)] = (
+            Tile(TileType.KEY, '@', self.__four_tiles[Point(startpoint.x - 1, startpoint.y - 1)].neighbors))
+        self.__four_keys['£'] = Point(startpoint.x + 1, startpoint.y - 1)
+        self.__four_tiles[Point(startpoint.x + 1, startpoint.y - 1)] = (
+            Tile(TileType.KEY, '£', self.__four_tiles[Point(startpoint.x + 1, startpoint.y - 1)].neighbors))
+        self.__four_keys['$'] = Point(startpoint.x - 1, startpoint.y + 1)
+        self.__four_tiles[Point(startpoint.x - 1, startpoint.y + 1)] = (
+            Tile(TileType.KEY, '$', self.__four_tiles[Point(startpoint.x - 1, startpoint.y + 1)].neighbors))
+        self.__four_keys['%'] = Point(startpoint.x + 1, startpoint.y + 1)
+        self.__four_tiles[Point(startpoint.x + 1, startpoint.y + 1)] = (
+            Tile(TileType.KEY, '%', self.__four_tiles[Point(startpoint.x + 1, startpoint.y + 1)].neighbors))
+
+    def get_steps_all_keys_one_room(self) -> int:
+        # Create dict of connections between keys
+        keypaths: dict[str: set[tuple[str, int, str]]] = {k: set() for k in self.__keys}  # key : key, steps, doors
+        for k in self.__keys:
+            seen = set()
+            queue = [(self.__keys[k], '', Point(-1, -1), 0)]
+            while queue:
+                current, doors, previous, steps = queue.pop(0)
+                if (current, doors) in seen:
+                    continue
+                seen.add((current, doors))
+                if self.__tiles[current].type == TileType.KEY and self.__tiles[current].value != k:
+                    keypaths[k].add((self.__tiles[current].value, steps, doors))
+                    continue
+                if self.__tiles[current].type == TileType.DOOR:
+                    doors = ''.join(sorted(doors + self.__tiles[current].value.lower()))
+                for n in self.__tiles[current].neighbors:
+                    if n != previous:
+                        queue.append((n, doors, current, steps + 1))
+        # Use the key connection dict for a Dijkstra search for shortest way to collect all keys
+        visited: dict = {}
+        pqueue = []
+        heappush(pqueue, (0, '@', ''))
+        while pqueue:
+            steps, current_key, have_keys = heappop(pqueue)
+            have_keys = ''.join(sorted(set(have_keys + current_key)))
+            if len(have_keys) == len(self.__keys):
+                return steps
+            if (current_key, have_keys) in visited and visited[(current_key, have_keys)] <= steps:
                 continue
-            seen.add((current, keys))
-            for n in self.__tiles[current].neighbors:
-                if ((self.__tiles[n].type != TileType.DOOR or self.__tiles[n].value.lower() in keys) and
-                        (n != previous or keyfound)):
-                    queue.append((n, keys, current, steps + 1))
+            visited[(current_key, have_keys)] = steps
+            for n, cost, doors in keypaths[current_key]:
+                if set(doors).issubset(set(have_keys)):
+                    heappush(pqueue, (steps + cost, n, have_keys))
+        return -1
+
+    def get_steps_all_keys_four_rooms(self) -> int:
+        # Create dict of connections between keys
+        keypaths: dict[str: set[tuple[str, int, str]]] = {k: set() for k in self.__four_keys}  # key : key, steps, doors
+        for k in self.__four_keys:
+            seen = set()
+            queue = [(self.__four_keys[k], '', Point(-1, -1), 0)]
+            while queue:
+                current, doors, previous, steps = queue.pop(0)
+                if (current, doors) in seen:
+                    continue
+                seen.add((current, doors))
+                if self.__four_tiles[current].type == TileType.KEY and self.__four_tiles[current].value != k:
+                    keypaths[k].add((self.__four_tiles[current].value, steps, doors))
+                    continue
+                if self.__four_tiles[current].type == TileType.DOOR:
+                    doors = ''.join(sorted(doors + self.__four_tiles[current].value.lower()))
+                for n in self.__four_tiles[current].neighbors:
+                    if n != previous:
+                        queue.append((n, doors, current, steps + 1))
+        # Use the key connection dict for a Dijkstra search for shortest way to collect all keys
+        visited: dict = {}
+        pqueue = []
+        heappush(pqueue, (0, ('@', '£', '$', '%'), ''))
+        while pqueue:
+            steps, current_key, have_keys = heappop(pqueue)
+            have_keys = ''.join(sorted(set(have_keys + ''.join(current_key))))
+            if len(have_keys) == len(self.__four_keys):
+                return steps
+            if (current_key, have_keys) in visited and visited[(current_key, have_keys)] <= steps:
+                continue
+            visited[(current_key, have_keys)] = steps
+            for i, ck in enumerate(current_key):
+                for n, cost, doors in keypaths[ck]:
+                    if set(doors).issubset(set(have_keys)):
+                        nextkey = list(current_key)
+                        nextkey[i] = n
+                        heappush(pqueue, (steps + cost, tuple(nextkey), have_keys))
         return -1
 
 
 def main() -> int:
     with open('../Inputfiles/aoc18.txt', 'r') as file:
         vault = Vault(file.read().strip('\n'))
-    print(f"Part 1: {vault.get_shortest_key_steps()}")
+    print(f"Part 1: {vault.get_steps_all_keys_one_room()}")
+    print(f"Part 2: {vault.get_steps_all_keys_four_rooms()}")
     return 0
 
 
