0320d419d96bd00ae940b2e8f3307511b4157e9d
BigMange <155587399+BigMange@users.noreply.github.com>
2024-07-22 20:48:58 +0200

    2020-day-14: Some restructuring.

--- 2020/Day_14/aoc14.py
+++ 2020/Day_14/aoc14.py
@@ -1,52 +1,73 @@
+"""
+Bitmask stuff and write into emulated memory. Not much to say.
+"""
 import sys
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Action(Enum):
+    MEM = 'mem'
+    MASK = 'mask'
+
+
+@dataclass(frozen=True)
+class Instruction:
+    action: Action
+    val1: str
+    val2: str = ''
 
 
 class Computer:
-    def __init__(self):
-        self.__bitmask = 0, 0
-        self.__memory: dict[int: int] = {}
-
-    def run_command(self, left: str, right: str) -> None:
-        if left == 'mask':
-            mask1 = ''
-            mask2 = ''
-            for i in range(len(right)):
-                if right[i] == 'X':
-                    mask1 += '1'
-                    mask2 += '0'
-                else:
-                    mask1 += '0'
-                    mask2 += right[i]
-            self.__bitmask = int(mask1, 2), int(mask2, 2)
-        else:
-            mem = int(left.strip('mem[').strip(']'))
-            val = int(right)
-            self.__memory[mem] = (val & self.__bitmask[0]) | self.__bitmask[1]
+    def __init__(self, rawstr: str) -> None:
+        self.__instr: list[Instruction] = []
+        for line in rawstr.splitlines():
+            left, right = line.split(' = ')
+            if left[:4] == 'mask':
+                self.__instr.append(Instruction(Action.MASK, right))
+            else:
+                self.__instr.append(Instruction(Action.MEM, left[4:].strip(']'), right))
 
     def get_memorysum(self) -> int:
-        return sum(list(self.__memory.values()))
+        memory: dict[int: int] = {}
+        bitmask = 0, 0
+        for instr in self.__instr:
+            if instr.action == Action.MASK:
+                mask1 = mask2 = ''
+                for c in instr.val1:
+                    if c == 'X':
+                        mask1 += '1'
+                        mask2 += '0'
+                    else:
+                        mask1 += '0'
+                        mask2 += c
+                bitmask = int(mask1, 2), int(mask2, 2)
+            else:
+                addr = int(instr.val1)
+                val = int(instr.val2)
+                memory[addr] = (val & bitmask[0]) | bitmask[1]
+        return sum(list(memory.values()))
 
-
-class ComputerV2:
-    def __init__(self):
-        self.__bitmask = ''
-        self.__xmask = 0
-        self.__memory: dict[int: int] = {}
-
-    def run_command(self, left: str, right: str) -> None:
-        if left == 'mask':
-            self.__bitmask = right
-            xmask = ''
-            for c in self.__bitmask:
-                if c == 'X':
-                    xmask += '0'
-                else:
-                    xmask += '1'
-            self.__xmask = int(xmask, 2)
-        else:
-            addr = int(left.strip('mem[').strip(']'))
-            for mask in self.__mask_value(self.__bitmask):
-                self.__memory[(addr & self.__xmask) | mask] = int(right)
+    def get_memorysum_v2(self) -> int:
+        memory: dict[int: int] = {}
+        bitmask = ''
+        xmask = 0
+        for instr in self.__instr:
+            if instr.action == Action.MASK:
+                bitmask = instr.val1
+                x = ''
+                for c in bitmask:
+                    if c == 'X':
+                        x += '0'
+                    else:
+                        x += '1'
+                xmask = int(x, 2)
+            elif instr.action == Action.MEM:
+                addr = int(instr.val1)
+                val = int(instr.val2)
+                for mask in self.__mask_value(bitmask):
+                    memory[(addr & xmask) | mask] = val
+        return sum(list(memory.values()))
 
     def __mask_value(self, val: str) -> list[int]:
         if (idx := val.find('X')) != -1:
@@ -55,20 +76,12 @@ class ComputerV2:
         else:
             return [int(val, 2)]
 
-    def get_memorysum(self) -> int:
-        return sum(list(self.__memory.values()))
-
 
 def main() -> int:
-    mycomputer = Computer()
-    myv2computer = ComputerV2()
-    with open('../Inputfiles/aoc14.txt') as file:
-        commands = file.read().strip('\n').splitlines()
-    for line in commands:
-        mycomputer.run_command(*line.split(' = '))
-        myv2computer.run_command(*line.split(' = '))
-    print("Part 1:", mycomputer.get_memorysum())
-    print("Part 2:", myv2computer.get_memorysum())
+    with open('../Inputfiles/aoc14.txt', 'r') as file:
+        computer = Computer(file.read().strip('\n'))
+    print(f"Part 1: {computer.get_memorysum()}")
+    print(f"Part 2: {computer.get_memorysum_v2()}")
     return 0
 
 
