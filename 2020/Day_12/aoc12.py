eb62f17c0cca2a174f5e0db36a21af8ff28b26de
BigMange <155587399+BigMange@users.noreply.github.com>
2024-07-22 20:02:56 +0200

    2020-day-12: Largely rewritten to something much cleaner.

--- 2020/Day_12/aoc12.py
+++ 2020/Day_12/aoc12.py
@@ -1,71 +1,78 @@
 """
-Create a ship class that holds the position and direction of the ship / waypoint.
-Possibly for refactoring - try to see if there is a way to merge the two ship variants.
+Use a point class to simplify coordinate handling, then it's mostly just a matter of going through the instructions
+and moving the ship accordingly.
 """
 import sys
+from enum import Enum
+from dataclasses import dataclass
 
-Directions = {'N': (0, 1), 'S': (0, -1), 'W': (-1, 0), 'E': (1, 0)}
-Actions = ('N', 'E', 'S', 'W', 'L', 'R', 'F')
+
+class Action(Enum):
+    LEFT = 'L'
+    RIGHT = 'R'
+    FORWARD = 'F'
+    NORTH = 'N'
+    SOUTH = 'S'
+    WEST = 'W'
+    EAST = 'E'
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+    def rotate(self, d: Action) -> "Point":
+        if d == Action.LEFT:
+            return Point(self.y, -self.x)
+        else:
+            return Point(-self.y, self.x)
+
+    def get_distance(self) -> int:
+        return abs(self.x) + abs(self.y)
+
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other: int) -> "Point":
+        return Point(self.x * other, self.y * other)
+
+
+@dataclass(frozen=True)
+class Instruction:
+    action: Action
+    value: int
 
 
 class Ship:
-    def __init__(self):
-        self.xy = 0, 0
-        self.direction = 'E'
+    __DIRECTIONMAP = {Action.EAST: Point(1, 0), Action.SOUTH: Point(0, 1),
+                      Action.WEST: Point(-1, 0), Action.NORTH: Point(0, -1)}
 
-    def perform_instruction(self, action: str, value: int) -> None:
-        if action in ('L', 'R'):
-            diridx = Actions.index(self.direction)
-            if action == 'R':
-                self.direction = Actions[(diridx + value // 90) % 4]
-            else:
-                self.direction = Actions[(diridx - value // 90) % 4]
-            return
-        direction = self.direction
-        if action in ('N', 'E', 'S', 'W'):
-            direction = action
-        self.xy = (self.xy[0] + (Directions[direction][0] * value), self.xy[1] + (Directions[direction][1] * value))
+    def __init__(self, rawstr: str) -> None:
+        self.__instructions = [Instruction(Action(line[0]), int(line[1:])) for line in rawstr.splitlines()]
 
-    def get_distance(self) -> int:
-        return abs(self.xy[0]) + abs(self.xy[1])
-
-
-class ShipV2(Ship):
-    def __init__(self):
-        super().__init__()
-        self.waypoint = (10, 1)
-
-    def perform_instruction(self, action: str, value: int) -> None:
-        if action in ('L', 'R'):
-            match (value // 90) % 4:
-                case 1:
-                    if action == 'R':
-                        self.waypoint = self.waypoint[1], -self.waypoint[0]
-                    else:
-                        self.waypoint = -self.waypoint[1], self.waypoint[0]
-                case 2:
-                    self.waypoint = -self.waypoint[0], -self.waypoint[1]
-                case 3:
-                    if action == 'L':
-                        self.waypoint = self.waypoint[1], -self.waypoint[0]
-                    else:
-                        self.waypoint = -self.waypoint[1], self.waypoint[0]
-        if action in ('N', 'E', 'S', 'W'):
-            self.waypoint = (self.waypoint[0] + (Directions[action][0] * value),
-                             self.waypoint[1] + (Directions[action][1] * value))
-        if action == 'F':
-            self.xy = self.xy[0] + (self.waypoint[0] * value), self.xy[1] + (self.waypoint[1] * value)
+    def get_distance(self, use_waypoint: bool = False) -> int:
+        position = Point(0, 0)
+        direction = Ship.__DIRECTIONMAP[Action.EAST] if not use_waypoint else Point(10, -1)
+        for instr in self.__instructions:
+            if instr.action in (Action.LEFT, Action.RIGHT):
+                for nbr in range(instr.value // 90):
+                    direction = direction.rotate(instr.action)
+            elif instr.action in (Action.NORTH, Action.EAST, Action.SOUTH, Action.WEST):
+                if not use_waypoint:
+                    position += Ship.__DIRECTIONMAP[instr.action] * instr.value
+                else:
+                    direction += Ship.__DIRECTIONMAP[instr.action] * instr.value
+            elif instr.action == Action.FORWARD:
+                position += direction * instr.value
+        return position.get_distance()
 
 
 def main() -> int:
-    myship = Ship()
-    myshipv2 = ShipV2()
     with open('../Inputfiles/aoc12.txt', 'r') as file:
-        for line in file.read().strip('\n').splitlines():
-            myship.perform_instruction(line[0], int(line[1:]))
-            myshipv2.perform_instruction(line[0], int(line[1:]))
-    print("Part 1:", myship.get_distance())
-    print("Part 2:", myshipv2.get_distance())
+        myship = Ship(file.read().strip('\n'))
+    print(f"Part 1: {myship.get_distance()}")
+    print(f"Part 2: {myship.get_distance(True)}")
     return 0
 
 
