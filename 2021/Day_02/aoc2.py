8ebf286e92c86653904135f5272bbd3c6c5cfefe
BigMange <155587399+BigMange@users.noreply.github.com>
2024-07-27 13:03:31 +0200

    2021-day-2: Some cleanup of the solution.

--- 2021/Day_02/aoc2.py
+++ 2021/Day_02/aoc2.py
@@ -1,60 +1,62 @@
 """
-Part 1: Create a submarine class to hold the coordinates and provide methods to execute the move commands and to
-calculate the final score.
-Part 2: Create a subclass to submarine which also holds the 'aim' and modifies the move command method.
+Straightforward, just go through the instructions and update the position according to the rules.
 """
 import sys
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Direction(Enum):
+    UP = 'up'
+    DOWN = 'down'
+    FORWARD = 'forward'
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
 
 
 class Submarine:
-    def __init__(self, init_x: int, init_y: int):
-        self.x = init_x
-        self.y = init_y
+    def __init__(self, rawstr: str) -> None:
+        self.__instructions = [(Direction(left), int(right)) for left, right in
+                               [line.split() for line in rawstr.splitlines()]]
 
-    def movecommand(self, direction: str, value: int):
-        match direction:
-            case 'up':
-                self.y -= value
-            case 'down':
-                self.y += value
-            case 'forward':
-                self.x += value
-            case _:
-                pass
+    def get_move_result(self) -> int:
+        position = Point(0, 0)
+        for direction, value in self.__instructions:
+            match direction:
+                case Direction.UP:
+                    position += Point(0, -value)
+                case Direction.DOWN:
+                    position += Point(0, value)
+                case Direction.FORWARD:
+                    position += Point(value, 0)
+        return position.x * position.y
 
-    def getscore(self) -> int:
-        return self.x * self.y
-
-
-class AimedSubmarine(Submarine):
-    def __init__(self, init_x: int, init_y: int):
-        self.__aim = 0
-        super().__init__(init_x, init_y)
-
-    def movecommand(self, direction: str, value: int):
-        match direction:
-            case 'up':
-                self.__aim -= value
-            case 'down':
-                self.__aim += value
-            case 'forward':
-                self.x += value
-                self.y += self.__aim * value
-            case _:
-                pass
+    def get_aimed_move_result(self) -> int:
+        position = Point(0, 0)
+        aim = 0
+        for direction, value in self.__instructions:
+            match direction:
+                case Direction.UP:
+                    aim -= value
+                case Direction.DOWN:
+                    aim += value
+                case Direction.FORWARD:
+                    position += Point(value, aim * value)
+        return position.x * position.y
 
 
 def main() -> int:
-    mysub = Submarine(0, 0)
-    myaimedsub = AimedSubmarine(0, 0)
     with open('../Inputfiles/aoc2.txt', 'r') as file:
-        lines = file.read().strip('\n').splitlines()
-    for line in lines:
-        direction, value = line.split()
-        mysub.movecommand(direction, int(value))
-        myaimedsub.movecommand(direction, int(value))
-    print(f"Part 1: {mysub.getscore()}")
-    print(f"Part 2: {myaimedsub.getscore()}")
+        submarine = Submarine(file.read().strip('\n'))
+    print(f"Part 1: {submarine.get_move_result()}")
+    print(f"Part 2: {submarine.get_aimed_move_result()}")
     return 0
 
 
