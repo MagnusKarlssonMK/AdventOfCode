e800b06bdee179eb072d62e85a328cc5b48654ee
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-08-08 22:56:53 +0200

    2022 day 3: Add some structure to the code.

--- 2022/day03.py
+++ 2022/day03.py
@@ -5,29 +5,36 @@ ROOT_DIR = Path(Path(__file__).parents[2], 'AdventOfCode-Input')
 INPUT_FILE = Path(ROOT_DIR, '2022/day03.txt')
 
 
-def main() -> int:
-    rangelist = ((range(ord('a'), ord('z') + 1), 1),
-                 (range(ord('A'), ord('Z') + 1), 27))
-    totalprio_p1 = 0
-    totalprio_p2 = 0
-    with open(INPUT_FILE, 'r') as file:
-        linebuffer = []
-        for line in file.read().strip('\n').splitlines():
-            linebuffer.append(line)
-            left, right = line[:len(line) // 2], line[len(line) // 2:]
-            shared_p1 = ''.join(set(left).intersection(right))
-            if len(shared_p1) > 0:
-                totalprio_p1 += sum([ord(shared_p1[0]) - r[0].start + r[1] for r in rangelist
-                                     if ord(shared_p1[0]) in r[0]])
-            if len(linebuffer) >= 3:
-                shared_p2 = ''.join(set(linebuffer[0]).intersection(linebuffer[1]).intersection(linebuffer[2]))
-                linebuffer.clear()
-                if len(shared_p2) > 0:
-                    totalprio_p2 += sum([ord(shared_p2[0]) - r[0].start + r[1] for r in rangelist
-                                         if ord(shared_p2[0]) in r[0]])
-    print(f"Part1: {totalprio_p1}")
-    print(f"Part2: {totalprio_p2}")
+class Supplies:
+    __RANGELIST = ((range(ord('a'), ord('z') + 1), 1), (range(ord('A'), ord('Z') + 1), 27))
 
+    def __init__(self, rawstr: str) -> None:
+        self.__rucksacks = rawstr.splitlines()
+
+    def get_total_prio_both_compartments(self) -> int:
+        result = 0
+        for rucksack in self.__rucksacks:
+            left, right = rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]
+            if len(shared := ''.join(set(left) & set(right))) > 0:
+                result += sum([ord(shared[0]) - r0.start + r1 for r0, r1 in Supplies.__RANGELIST
+                               if ord(shared[0]) in r0])
+        return result
+
+    def get_total_prio_group(self) -> int:
+        result = 0
+        for r in range(0, len(self.__rucksacks), 3):
+            s0, s1, s2 = self.__rucksacks[r: r + 3]
+            if len(shared := ''.join(set(s0) & set(s1) & set(s2))) > 0:
+                result += sum([ord(shared[0]) - r0.start + r1 for r0, r1 in Supplies.__RANGELIST
+                               if ord(shared[0]) in r0])
+        return result
+
+
+def main() -> int:
+    with open(INPUT_FILE, 'r') as file:
+        supplies = Supplies(file.read().strip('\n'))
+    print(f"Part 1: {supplies.get_total_prio_both_compartments()}")
+    print(f"Part 2: {supplies.get_total_prio_group()}")
     return 0
 
 
