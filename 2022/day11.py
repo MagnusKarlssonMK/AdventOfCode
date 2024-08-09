4e6575ab66c68190ceb1297121af10ca9818c8fa
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-08-09 16:41:13 +0200

    2022 day 11: Move things out of main.

--- 2022/day11.py
+++ 2022/day11.py
@@ -8,74 +8,66 @@ INPUT_FILE = Path(ROOT_DIR, '2022/day11.txt')
 
 
 class Monkey:
-    def __init__(self, rawstr: str, applyrelief: bool) -> None:
+    def __init__(self, rawstr: str) -> None:
         lines = rawstr.splitlines()
-        self.monkeyid = int(re.findall(r'\d+', lines[0])[0])
-        self.items = list(map(int, re.findall(r'\d+', lines[1])))
-        _, self.operation = lines[2].split(' = ')
-        self.testdiv = int(re.findall(r'\d+', lines[3])[0])
-        self.truemonkey = int(re.findall(r'\d+', lines[4])[0])
-        self.falsemonkey = int(re.findall(r'\d+', lines[5])[0])
-        self.inspectioncount = 0
-        self.applyrelief = applyrelief
+        self.__startitems = tuple(map(int, re.findall(r"\d+", lines[1])))
+        op = lines[2].split(' = ')[-1].split()
+        self.__operation = op[1]
+        self.__op_nbrs = (op[0], op[2])
+        self.test = int(lines[3].split()[-1])
+        self.__destination_pass = int(lines[4].split()[-1])
+        self.__destination_fail = int(lines[5].split()[-1])
+        self.__current_items = list(self.__startitems)
+        self.inpection_count = 0
 
-    def inspectitems(self, lcm: int = 1) -> iter:  # Monkey ID, item level
-        retlist = []
-        while len(self.items) > 0:
-            currentitem = self.items.pop(0)
-            self.inspectioncount += 1
-            left, op, right = self.operation.split()
-            nbr = [0, 0]
-            for i, n in enumerate([left, right]):
-                nbr[i] = currentitem if not n.isdigit() else int(n)
-            if op == '+':
-                currentitem = nbr[0] + nbr[1]
-            elif op == '*':
-                currentitem = nbr[0] * nbr[1]
-            if self.applyrelief:
-                currentitem = currentitem // 3
+    def reset(self) -> None:
+        self.__current_items = list(self.__startitems)
+        self.inpection_count = 0
+
+    def inspect(self, apply_relief: bool, lcm: int) -> iter:  # Destination monkey ID, item level
+        while self.__current_items:
+            current_item = self.__current_items.pop(0)
+            self.inpection_count += 1
+            nbrs = [current_item if not n.isdigit() else int(n) for n in self.__op_nbrs]
+            if self.__operation == '*':
+                current_item = nbrs[0] * nbrs[1]
+            elif self.__operation == '+':
+                current_item = sum(nbrs)
+            if apply_relief:
+                current_item //= 3
             else:
-                currentitem %= lcm
-            if currentitem % self.testdiv == 0:
-                yield self.truemonkey, currentitem
+                current_item %= lcm
+            if current_item % self.test == 0:
+                yield self.__destination_pass, current_item
             else:
-                yield self.falsemonkey, currentitem
-        return retlist
+                yield self.__destination_fail, current_item
 
-    def catchitem(self, newitem: int) -> None:
-        self.items.append(newitem)
+    def catch_item(self, new_item: int) -> None:
+        self.__current_items.append(new_item)
 
-    def __str__(self):
-        return (f"ID: {self.monkeyid}\n   Items: {self.items}\n   Op: {self.operation}\n   Testdiv: {self.testdiv}\n   "
-                f"True: {self.truemonkey}\n   False: {self.falsemonkey}\n   Count: {self.inspectioncount}\n")
+
+class KeepAway:
+    def __init__(self, rawstr: str) -> None:
+        self.__monkeys: dict[int: Monkey] = {}
+        for i, block in enumerate(rawstr.split('\n\n')):
+            self.__monkeys[i] = Monkey(block)
+
+    def get_monkey_level(self, rounds: int = 20, apply_relief: bool = True) -> int:
+        lcm = 1 if apply_relief else math.lcm(*[m.test for m in self.__monkeys.values()])
+        for _ in range(rounds):
+            for monkey in self.__monkeys:
+                for destination, item in self.__monkeys[monkey].inspect(apply_relief, lcm):
+                    self.__monkeys[destination].catch_item(item)
+        result = sorted([m.inpection_count for m in self.__monkeys.values()], reverse=True)
+        [self.__monkeys[m].reset() for m in self.__monkeys]
+        return result[0] * result[1]
 
 
 def main() -> int:
-    monkeys_p1: list[Monkey] = []
-    monkeys_p2: list[Monkey] = []
     with open(INPUT_FILE, 'r') as file:
-        for monk in file.read().strip('\n').split('\n\n'):
-            monkeys_p1.append(Monkey(monk, True))
-            monkeys_p2.append(Monkey(monk, False))
-
-    for _ in range(20):
-        for monkey in monkeys_p1:
-            for item in monkey.inspectitems():
-                monkeys_p1[item[0]].catchitem(item[1])
-
-    countlist = sorted([monkey.inspectioncount for monkey in monkeys_p1], reverse=True)
-    print(f"Part 1: {countlist[0] * countlist[1]}")
-
-    lcm = math.lcm(*[m.testdiv for m in monkeys_p2])
-
-    for _ in range(10000):
-        for monkey in monkeys_p2:
-            for item in monkey.inspectitems(lcm):
-                monkeys_p2[item[0]].catchitem(item[1])
-
-    countlist = sorted([monkey.inspectioncount for monkey in monkeys_p2], reverse=True)
-    print(f"Part 2: {countlist[0] * countlist[1]}")
-
+        game = KeepAway(file.read().strip('\n'))
+    print(f"Part 1: {game.get_monkey_level()}")
+    print(f"Part 2: {game.get_monkey_level(10_000, False)}")
     return 0
 
 
