08da85545e0d713fa9fb8026943e63afb185d0ca
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-08-09 17:19:22 +0200

    2022 day 12: Use point class to represent coordinates.

--- 2022/day12.py
+++ 2022/day12.py
@@ -3,83 +3,87 @@ Part 1 - loads the grid into a grid class, and then finding the path is pretty m
 condition checks when looking up the neighbors.
 Part 2 - similar to Part 1, but this instead does the BFS 'backwards' starting from the end point and also reverses
 the condition in the neighbor check.
-Possible improvement: the grid class methods for part 1 and 2 can probably be merged, since most of the code is the
-same.
 """
 import sys
 from pathlib import Path
+from dataclasses import dataclass
 
 ROOT_DIR = Path(Path(__file__).parents[2], 'AdventOfCode-Input')
 INPUT_FILE = Path(ROOT_DIR, '2022/day12.txt')
 
 
-RowCol = tuple[int, int]
+@dataclass(frozen=True)
+class Point:
+    row: int
+    col: int
+
+    def get_neighbors(self) -> iter:
+        for d in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+            yield self + Point(*d)
+
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.row + other.row, self.col + other.col)
 
 
 class Grid:
-    def __init__(self, inputstring: str):
-        self.grid: list[str] = []
-        [self.grid.append(line) for line in inputstring.splitlines()]
-        self.height = len(self.grid)
-        self.width = len(self.grid[0])
-        self.startpos: RowCol = 0, 0
-        self.endpos: RowCol = 0, 0
-        for row in range(self.height):
-            if (startcol := self.grid[row].find('S')) >= 0:
-                self.startpos = row, startcol
-                self.grid[row] = self.grid[row].replace('S', 'a')
-            if (endcol := self.grid[row].find('E')) >= 0:
-                self.endpos = row, endcol
-                self.grid[row] = self.grid[row].replace('E', 'z')
+    def __init__(self, rawstr: str):
+        self.__grid: list[str] = [line for line in rawstr.splitlines()]
+        self.__height = len(self.__grid)
+        self.__width = len(self.__grid[0])
+        self.__startpos: Point = Point(0, 0)
+        self.__endpos: Point = Point(0, 0)
+        for row in range(self.__height):
+            if (startcol := self.__grid[row].find('S')) >= 0:
+                self.__startpos = Point(row, startcol)
+                self.__grid[row] = self.__grid[row].replace('S', 'a')
+            if (endcol := self.__grid[row].find('E')) >= 0:
+                self.__endpos = Point(row, endcol)
+                self.__grid[row] = self.__grid[row].replace('E', 'z')
 
-    def getneigbors(self, coord: RowCol, downhill: bool = False) -> iter:
-        row, col = coord
-        for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            if 0 <= row + dr < self.height and 0 <= col + dc < self.width:
-                current = ord(self.grid[row][col])
-                candidate = ord(self.grid[row + dr][col + dc])
+    def getneigbors(self, coord: Point, downhill: bool = False) -> iter:
+        for n in coord.get_neighbors():
+            if 0 <= n.row < self.__height and 0 <= n.col < self.__width:
+                current = ord(self.__grid[coord.row][coord.col])
+                candidate = ord(self.__grid[n.row][n.col])
                 if (current + 1 >= candidate and not downhill) or (current <= candidate + 1 and downhill):
-                    yield row + dr, col + dc
+                    yield n
 
     def get_minsteps_fromstart(self) -> int:
         # Part 1: Regular BFS search from S to E
-        visited: dict[RowCol: (int, RowCol)] = {}
-        tilequeue: list[tuple[RowCol, int, RowCol]] = [(self.startpos, 0, None)]
-        while len(tilequeue) > 0:
-            current_pos, current_steps, previous = tilequeue.pop(0)
+        visited: dict[Point: (int, Point)] = {}
+        tilequeue: list[tuple[Point, int]] = [(self.__startpos, 0)]
+        while tilequeue:
+            current_pos, current_steps = tilequeue.pop(0)
+            if current_pos == self.__endpos:
+                return current_steps
             if current_pos in visited:
                 continue
             for neighbor in self.getneigbors(current_pos):
                 if neighbor not in visited:
-                    tilequeue.append((neighbor, current_steps + 1, current_pos))
-            visited[current_pos] = current_steps, previous
-            if self.endpos in visited:
-                break
-        return visited[self.endpos][0]
+                    tilequeue.append((neighbor, current_steps + 1))
+            visited[current_pos] = current_steps
+        return -1
 
     def get_minsteps_fromany(self) -> int:
         # Part 2: BFS again but starting from E and going downhill until finding the first 'a'
-        visited: dict[RowCol: (int, RowCol)] = {}
-        tilequeue: list[tuple[RowCol, int, RowCol]] = [(self.endpos, 0, None)]
-        beststart = -1, -1
-        while len(tilequeue) > 0:
-            current_pos, current_steps, previous = tilequeue.pop(0)
+        visited: dict[Point: (int, Point)] = {}
+        tilequeue: list[tuple[Point, int]] = [(self.__endpos, 0)]
+        while tilequeue:
+            current_pos, current_steps = tilequeue.pop(0)
+            if self.__grid[current_pos.row][current_pos.col] == 'a':
+                return current_steps
             if current_pos in visited:
                 continue
             for neighbor in self.getneigbors(current_pos, True):
                 if neighbor not in visited:
-                    tilequeue.append((neighbor, current_steps + 1, current_pos))
-            visited[current_pos] = current_steps, previous
-            if self.grid[current_pos[0]][current_pos[1]] == 'a':
-                beststart = current_pos
-                break
-        return visited[beststart][0]
+                    tilequeue.append((neighbor, current_steps + 1))
+            visited[current_pos] = current_steps
+        return -1
 
 
 def main() -> int:
     with open(INPUT_FILE, 'r') as file:
         mygrid = Grid(file.read().strip('\n'))
-
     print(f"Part 1: {mygrid.get_minsteps_fromstart()}")
     print(f"Part 2: {mygrid.get_minsteps_fromany()}")
     return 0
