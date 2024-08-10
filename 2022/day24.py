a85c7b50ec31e3ba9b16ce159bd36cf7069aafaa
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-08-10 12:17:29 +0200

    2022 day 24: Use point class to represent coordinates, and use A* with manhattan distance for faster search.

--- 2022/day24.py
+++ 2022/day24.py
@@ -1,87 +1,115 @@
 """
 To avoid having to continuously update the list of blizzards, instead store the blizzards in a per-direction dict,
 and calculate dynamically whether a certain point will be occupied by at least one blizzard at a certain minute.
-Use BFS to traverse the map. We need to allow backtracking, but have a check that we only evaluate a certain point
-once at a specific time (it doesn't matter how we got there).
+Use A* to traverse the map and find the shortest path, using the manhattan distance to the target as heuristic. The
+time spent is effectively a third dimension in the space, since the possible neighbors changes with time, so we need
+to include both position and time as key for the visited nodes.
+The map state is cyclic since the winds will eventually return to the start position (lcm of width, height), but the
+cycle is longer than the time spent to traverse the map, so there isn't much of a benefit to optimize making use of the
+cycle.
 """
 import sys
 from pathlib import Path
+from dataclasses import dataclass
+from enum import Enum
+from heapq import heappop, heappush
 
 ROOT_DIR = Path(Path(__file__).parents[2], 'AdventOfCode-Input')
 INPUT_FILE = Path(ROOT_DIR, '2022/day24.txt')
 
 
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+    def get_steps(self) -> iter:
+        for d in Direction:
+            yield self + d.value
+        yield self
+
+    def manhattan(self, other: "Point") -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Point") -> "Point":
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __lt__(self, other: "Point") -> bool:
+        return self.y < other.y if self.y != other.y else self.x < other.x
+
+
+class Direction(Enum):
+    RIGHT = Point(1, 0)
+    DOWN = Point(0, 1)
+    LEFT = Point(-1, 0)
+    UP = Point(0, -1)
+
+
 class Valley:
-    def __init__(self, inputgrid: str) -> None:
-        directions = {'>': (0, 1), '<': (0, -1), '^': (-1, 0), 'v': (1, 0)}
-        self.__startpoint = -1, -1
-        self.__exitpoint = -1, -1
-        self.__walls = set()
-        self.__blizzards: dict[tuple[int, int]: set[tuple[int, int]]] = {directions[d]: set() for d in directions}
-        lines = inputgrid.splitlines()
+    def __init__(self, rawstr: str) -> None:
+        dirmap = {'>': Direction.RIGHT, '<': Direction.LEFT, '^': Direction.UP, 'v': Direction.DOWN}
+        self.__startpoint = Point(-1, -1)
+        self.__exitpoint = Point(-1, -1)
+        self.__walls: set[Point] = set()
+        self.__blizzards: dict[Direction: set[Point]] = {d: set() for d in Direction}
+        lines = rawstr.splitlines()
         self.__width = len(lines[0]) - 2  # Don't include the walls
         self.__height = len(lines) - 2
-        for r, row in enumerate(lines):
-            for c, col in enumerate(row):
-                if col == "#":
-                    self.__walls.add((r, c))
-                elif r == 0 and col == ".":
-                    self.__startpoint = (r, c)
-                elif r == len(lines) - 1 and col == ".":
-                    self.__exitpoint = (r, c)
-                elif col in directions:
-                    self.__blizzards[directions[col]].add((r, c))
+        for y, line in enumerate(lines):
+            for x, c in enumerate(line):
+                if c == '#':
+                    self.__walls.add(Point(x, y))
+                elif y == 0 and c == '.':
+                    self.__startpoint = Point(x, y)
+                elif y == self.__height + 1 and c == '.':
+                    self.__exitpoint = Point(x, y)
+                elif c in dirmap:
+                    self.__blizzards[dirmap[c]].add(Point(x, y))
 
-    def __is_occupied_at_step(self, point_row: int, point_col: int, step: int) -> bool:
-        for bliz_dir_row, bliz_dir_col in self.__blizzards:
-            start_row = 1 + (point_row - 1 - (step * bliz_dir_row)) % self.__height
-            start_col = 1 + (point_col - 1 - (step * bliz_dir_col)) % self.__width
-            if (start_row, start_col) in self.__blizzards[bliz_dir_row, bliz_dir_col]:
+    def __is_occupied_at_step(self, point: Point, step: int) -> bool:
+        if point == self.__startpoint or point == self.__exitpoint:
+            return False
+        for direction in self.__blizzards:
+            start_x = 1 + (point.x - 1 - (step * direction.value.x)) % self.__width
+            start_y = 1 + (point.y - 1 - (step * direction.value.y)) % self.__height
+            if Point(start_x, start_y) in self.__blizzards[direction]:
                 return True
         return False
 
-    def __bfs(self, start: tuple[int, int], end: tuple[int, int], startstep: int = 0) -> int:
-        queue = [(start, startstep)]
+    def __shortest_path(self, start: Point, end: Point, startstep: int = 0) -> int:
+        queue = []
+        heappush(queue, (0, start, startstep))
         seen = set()
         while queue:
-            point, steps = queue.pop(0)
+            _, point, steps = heappop(queue)
+            if point == end:
+                return steps
             if (point, steps) in seen:
                 continue
             seen.add((point, steps))
-            point_row, point_col = point
-            for d_row, d_col in [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]:
-                new_row = point_row + d_row
-                new_col = point_col + d_col
-                new_point = (new_row, new_col)
-                next_step = steps + 1
-                if new_point == start and (d_row, d_col) != start:
-                    # The start point won't pass the boundary check below, and the occupied check won't work for it
-                    # either. So just put it on the queue directly as a workaround.
-                    queue.append((new_point, next_step))
-                    continue
-                if new_point == end:
-                    return next_step
-                if 0 < new_row <= self.__height and 0 < new_col <= self.__width:
-                    if not self.__is_occupied_at_step(new_row, new_col, next_step):
-                        queue.append((new_point, next_step))
+            for next_step in point.get_steps():
+                if ((0 < next_step.y <= self.__height and 0 < next_step.x <= self.__width) or
+                        next_step in (start, end)):
+                    if not self.__is_occupied_at_step(next_step, steps + 1):
+                        heappush(queue, (steps + 1 + next_step.manhattan(end), next_step, steps + 1))
         return -1
 
-    def get_there(self) -> int:
-        return self.__bfs(self.__startpoint, self.__exitpoint)
-
-    def get_there_and_back_again(self) -> int:
-        a = self.__bfs(self.__startpoint, self.__exitpoint, 0)
-        # Could have perhaps cached the 'a' value from the part 1 run
-        b = self.__bfs(self.__exitpoint, self.__startpoint, a)
-        c = self.__bfs(self.__startpoint, self.__exitpoint, b)
-        return c
+    def get_there_and_back_again(self) -> tuple[int, int]:
+        a = self.__shortest_path(self.__startpoint, self.__exitpoint, 0)
+        b = self.__shortest_path(self.__exitpoint, self.__startpoint, a)
+        c = self.__shortest_path(self.__startpoint, self.__exitpoint, b)
+        return a, c
 
 
 def main() -> int:
     with open(INPUT_FILE, 'r') as file:
         valley = Valley(file.read().strip('\n'))
-    print(f"Part 1: {valley.get_there()}")
-    print(f"Part 2: {valley.get_there_and_back_again()}")
+    p1, p2 = valley.get_there_and_back_again()
+    print(f"Part 1: {p1}")
+    print(f"Part 2: {p2}")
     return 0
 
 
