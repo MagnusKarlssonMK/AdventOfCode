c2d4c72679ee8d01b2437edd9bbcbc068c14b5c2
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-11-05 16:58:13 +0100

    Add some more unit tests.

--- 2017/tests/test_day04.py
+++ 2017/tests/test_day04.py
@@ -35,11 +35,3 @@ def test_part2_4() -> None:
 def test_part2_5() -> None:
     test_input = PassphraseList("oiii ioii iioi iiio")
     assert test_input.get_valid_count(True) == 0
-
-"""
-abcde fghij is a valid passphrase.
-abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
-a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
-iiii oiii ooii oooi oooo is valid.
-oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
-"""
