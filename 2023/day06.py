429264e73860440ff58d1872e480a89207738fec
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2025-09-02 22:38:19 +0200

    2023 day 6 Cleanup

--- 2023/day06.py
+++ 2023/day06.py
@@ -3,11 +3,11 @@ Uses quadratic formula to calculate the two points where the score intersects th
 challenges was to round them off in the right direction, to also account for the few cases where the solution was
 exactly the same value as the old record.
 """
+
 import time
 from pathlib import Path
 from dataclasses import dataclass
 import math
-import re
 
 
 @dataclass(frozen=True)
@@ -16,40 +16,48 @@ class Race:
     distance: int
 
     def get_score(self) -> int:
-        minvelocity = math.floor((self.time - math.sqrt(self.time**2 - (4 * self.distance))) / 2) + 1
-        maxvelocity = math.ceil((self.time + math.sqrt(self.time**2 - (4 * self.distance))) / 2) - 1
+        minvelocity = (
+            math.floor((self.time - math.sqrt(self.time**2 - (4 * self.distance))) / 2)
+            + 1
+        )
+        maxvelocity = (
+            math.ceil((self.time + math.sqrt(self.time**2 - (4 * self.distance))) / 2)
+            - 1
+        )
         return 1 + maxvelocity - minvelocity
 
 
-class Competition:
-    def __init__(self, rawstr: str) -> None:
-        t, d = rawstr.splitlines()
-        timelist = list(map(int, re.findall(r"\d+", t)))
-        distancelist = list(map(int, re.findall(r"\d+", d)))
-        self.__races = [Race(timelist[idx], distancelist[idx]) for idx, _ in enumerate(timelist)]
+class InputData:
+    def __init__(self, s: str) -> None:
+        t, d = s.splitlines()
+        timelist = list(map(int, t.split()[1:]))
+        distancelist = list(map(int, d.split()[1:]))
+        self.__races = [
+            Race(timelist[idx], distancelist[idx]) for idx, _ in enumerate(timelist)
+        ]
 
-    def get_race_product(self) -> int:
+    def solve_part1(self) -> int:
         return math.prod([race.get_score() for race in self.__races])
 
-    def get_megarace_score(self) -> int:
-        t = int(''.join([str(race.time) for race in self.__races]))
-        d = int(''.join([str(race.distance) for race in self.__races]))
+    def solve_part2(self) -> int:
+        t = int("".join([str(race.time) for race in self.__races]))
+        d = int("".join([str(race.distance) for race in self.__races]))
         megarace = Race(t, d)
         return megarace.get_score()
 
 
 def main(aoc_input: str) -> None:
-    myrace = Competition(aoc_input)
-    print(f"Part 1: {myrace.get_race_product()}")
-    print(f"Part 2: {myrace.get_megarace_score()}")
+    myrace = InputData(aoc_input)
+    print(f"Part 1: {myrace.solve_part1()}")
+    print(f"Part 2: {myrace.solve_part2()}")
 
 
 if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2023/day06.txt')
+    ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
+    INPUT_FILE = Path(ROOT_DIR, "2023/day06.txt")
 
     start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
+    with open(INPUT_FILE, "r") as file:
+        main(file.read().strip("\n"))
     end_time = time.perf_counter()
     print(f"Total time (ms): {1000 * (end_time - start_time)}")
--- 2023/tests/test_day06.py
+++ 2023/tests/test_day06.py
@@ -1,17 +1,21 @@
-from day06 import Competition
+from day06 import InputData
+
+
+TEST_DATA = """Time:      7  15   30
+Distance:  9  40  200"""
+
 
 # ----------- Part 1 ------------
 
+
 def test_part1_1() -> None:
-    test_string = '''Time:      7  15   30
-Distance:  9  40  200'''
-    test_input = Competition(test_string)
-    assert test_input.get_race_product() == 288
+    test_input = InputData(TEST_DATA)
+    assert test_input.solve_part1() == 288
+
 
 # ----------- Part 2 ------------
 
+
 def test_part2_1() -> None:
-    test_string = '''Time:      7  15   30
-Distance:  9  40  200'''
-    test_input = Competition(test_string)
-    assert test_input.get_megarace_score() == 71503
+    test_input = InputData(TEST_DATA)
+    assert test_input.solve_part2() == 71503
