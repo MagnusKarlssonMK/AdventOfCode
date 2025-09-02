f600e485a2fd33495d8106c46ede3e178bd555a0
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2025-09-03 00:24:15 +0200

    2023 day 9 Tidy

--- 2023/day09.py
+++ 2023/day09.py
@@ -2,6 +2,7 @@
 Just a simple recursive function to generate the lines until a line of zeroes shows up.
 Surprisingly simple part 2, basically just to reverse the input data and run the same function.
 """
+
 import time
 from pathlib import Path
 
@@ -14,10 +15,10 @@ def findnextnumber(nbrs: list[int]) -> int:
         return nbrs[-1] + findnextnumber(nextlevellist)
 
 
-def get_numbers(rawstr: str) -> tuple[int, int]:
+def get_numbers(s: str) -> tuple[int, int]:
     result_p1 = 0
     result_p2 = 0
-    for line in rawstr.splitlines():
+    for line in s.splitlines():
         numbers = list(map(int, line.split()))
         result_p1 += findnextnumber(numbers)
         result_p2 += findnextnumber(list(reversed(numbers)))
@@ -30,12 +31,12 @@ def main(aoc_input: str) -> None:
     print(f"Part2: {p2}")
 
 
-if __name__ == '__main__':
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2023/day09.txt')
+if __name__ == "__main__":
+    ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
+    INPUT_FILE = Path(ROOT_DIR, "2023/day09.txt")
 
     start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
+    with open(INPUT_FILE, "r") as file:
+        main(file.read().strip("\n"))
     end_time = time.perf_counter()
     print(f"Total time (ms): {1000 * (end_time - start_time)}")
