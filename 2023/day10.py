79e5e6cb9430999c365fd13aef0bdb8a316697a1
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2025-09-03 00:36:51 +0200

    2023 day 10 Tidy.

--- 2023/day10.py
+++ 2023/day10.py
@@ -3,48 +3,62 @@ Store the data in a grid, then for Part 1 simply walk through the system until r
 the answer by dividing the number of steps taken by 2. For part 2, calculate the answer by first determining the area
 with the shoelace formula and then use that with Pick's theorem.
 """
+
 import time
 from pathlib import Path
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class Coord:
-    row: int
-    col: int
+class Point:
+    x: int
+    y: int
 
-    def __add__(self, other: "Coord") -> "Coord":
-        return Coord(self.row + other.row, self.col + other.col)
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
 
-    def get_reverse(self) -> "Coord":
-        return Coord(self.row * -1, self.col * -1)
+    def get_reverse(self) -> "Point":
+        return Point(self.x * -1, self.y * -1)
 
 
 class Maze:
-    __DIRECTIONS: dict[str, Coord] = {'u': Coord(-1, 0), 'r': Coord(0, 1), 'd': Coord(1, 0), 'l': Coord(0, -1)}
-    __PIPES: dict[str, tuple[str, str]] = {'|': ('u', 'd'), '-': ('l', 'r'), 'L': ('u', 'r'),
-                                           'J': ('u', 'l'), '7': ('d', 'l'), 'F': ('d', 'r')}
+    __DIRECTIONS: dict[str, Point] = {
+        "u": Point(0, -1),
+        "r": Point(1, 0),
+        "d": Point(0, 1),
+        "l": Point(-1, 0),
+    }
+    __PIPES: dict[str, tuple[str, str]] = {
+        "|": ("u", "d"),
+        "-": ("l", "r"),
+        "L": ("u", "r"),
+        "J": ("u", "l"),
+        "7": ("d", "l"),
+        "F": ("d", "r"),
+    }
 
     def __init__(self, rawstr: str) -> None:
         self.__grid = rawstr.splitlines()
-        self.__startpoint: Coord = Coord(-1, -1)
-        for rowidx, row in enumerate(self.__grid):
-            if (idx := row.find('S')) >= 0:
-                self.__startpoint = Coord(rowidx, idx)
+        self.__startpoint: Point = Point(-1, -1)
+        for y, row in enumerate(self.__grid):
+            if (x := row.find("S")) >= 0:
+                self.__startpoint = Point(x, y)
         # Note: when starting at 'S', take whatever direction we find first, it doesn't matter which way we walk
         for direction in Maze.__DIRECTIONS.values():
             v = self.__get_value(self.__startpoint + direction)
-            if v == '.':
+            if v == ".":
                 continue
-            if direction.get_reverse() in [Maze.__DIRECTIONS[p] for p in Maze.__PIPES[v]]:
+            if direction.get_reverse() in [
+                Maze.__DIRECTIONS[p] for p in Maze.__PIPES[v]
+            ]:
                 self.__startdirection = direction
                 break
-        self.__pipepath: list[Coord] = []
+        self.__pipepath: list[Point] = []
 
-    def __get_value(self, pos: Coord) -> str:
-        return self.__grid[pos.row][pos.col]
+    def __get_value(self, pos: Point) -> str:
+        return self.__grid[pos.y][pos.x]
 
-    def __get_nextstepdir(self, pos: Coord, indir: Coord) -> Coord:
+    def __get_nextstepdir(self, pos: Point, indir: Point) -> Point:
         for outdir in Maze.__PIPES[self.__get_value(pos)]:
             if Maze.__DIRECTIONS[outdir] != indir.get_reverse():
                 return Maze.__DIRECTIONS[outdir]
@@ -72,8 +86,13 @@ class Maze:
         # Calculate shoelace area
         area = 0
         for idx, _ in enumerate(self.__pipepath):
-            area += ((self.__pipepath[idx].col * self.__pipepath[(idx + 1) % len(self.__pipepath)].row) -
-                     (self.__pipepath[(idx + 1) % len(self.__pipepath)].col * self.__pipepath[idx].row))
+            area += (
+                self.__pipepath[idx].x
+                * self.__pipepath[(idx + 1) % len(self.__pipepath)].y
+            ) - (
+                self.__pipepath[(idx + 1) % len(self.__pipepath)].x
+                * self.__pipepath[idx].y
+            )
             # Note: we need to 'close the loop' and include also the combination of the first and last entries, thus
             # mod length for the idx + 1 point
         area = abs(area) // 2
@@ -88,11 +107,11 @@ def main(aoc_input: str) -> None:
 
 
 if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2023/day10.txt')
+    ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
+    INPUT_FILE = Path(ROOT_DIR, "2023/day10.txt")
 
     start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
+    with open(INPUT_FILE, "r") as file:
+        main(file.read().strip("\n"))
     end_time = time.perf_counter()
     print(f"Total time (ms): {1000 * (end_time - start_time)}")
--- 2023/tests/test_day10.py
+++ 2023/tests/test_day10.py
@@ -2,28 +2,32 @@ from day10 import Maze
 
 # ----------- Part 1 ------------
 
+
 def test_part1_1() -> None:
-    test_string = '''.....
+    test_string = """.....
 .S-7.
 .|.|.
 .L-J.
-.....'''
+....."""
     test_input = Maze(test_string)
     assert test_input.get_farpoint_length() == 4
 
+
 def test_part1_2() -> None:
-    test_string = '''..F7.
+    test_string = """..F7.
 .FJ|.
 SJ.L7
 |F--J
-LJ...'''
+LJ..."""
     test_input = Maze(test_string)
     assert test_input.get_farpoint_length() == 8
 
+
 # ----------- Part 2 ------------
 
+
 def test_part2_1() -> None:
-    test_string = '''...........
+    test_string = """...........
 .S-------7.
 .|F-----7|.
 .||.....||.
@@ -31,12 +35,13 @@ def test_part2_1() -> None:
 .|L-7.F-J|.
 .|..|.|..|.
 .L--J.L--J.
-...........'''
+..........."""
     test_input = Maze(test_string)
     assert test_input.get_enclosed_count() == 4
 
+
 def test_part2_2() -> None:
-    test_string = '''.F----7F7F7F7F-7....
+    test_string = """.F----7F7F7F7F-7....
 .|F--7||||||||FJ....
 .||.FJ||||||||L7....
 FJL7L7LJLJ||LJ.L-7..
@@ -45,12 +50,13 @@ L--J.L7...LJS7F-7L7.
 ....L7.F7||L7|.L7L7|
 .....|FJLJ|FJ|F7|.LJ
 ....FJL-7.||.||||...
-....L---J.LJ.LJLJ...'''
+....L---J.LJ.LJLJ..."""
     test_input = Maze(test_string)
     assert test_input.get_enclosed_count() == 8
 
+
 def test_part2_3() -> None:
-    test_string = '''FF7FSF7F7F7F7F7F---7
+    test_string = """FF7FSF7F7F7F7F7F---7
 L|LJ||||||||||||F--J
 FL-7LJLJ||||||LJL-77
 F--JF--7||LJLJ7F7FJ-
@@ -59,6 +65,6 @@ L---JF-JLJ.||-FJLJJ7
 |FFJF7L7F-JF7|JL---7
 7-L-JL7||F7|L7F-7F7|
 L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L'''
+L7JLJL-JLJLJL--JLJ.L"""
     test_input = Maze(test_string)
     assert test_input.get_enclosed_count() == 10
