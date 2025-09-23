69943de852d8e4977bc0fd73015b277449a2d5d6
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2025-09-23 21:08:20 +0200

    2016 day 4 refactor and add unit test.

--- 2016/day04.py
+++ 2016/day04.py
@@ -1,20 +1,24 @@
 """
-Do most of the heavy lifting in a Room class, which will hold the input values and provides functions for validating
-the checksum and decodes the string.
+2016 day 4 - Security Through Obscurity
 """
 import time
 from pathlib import Path
-import re
 
 
 class Room:
-    def __init__(self, words: list[str], sectorid: int, checksum: str) -> None:
+    def __init__(self, s: str) -> None:
+        self.__sector_id = 0
+        self.__checksum = ""
+        parts = s.split('-')
+        words = [p for p in parts if ']' not in p]
+        left, right = parts[-1].split('[')
+        self.__sector_id = int(left)
+        self.__checksum = right.strip(']')
         self.__words = tuple(words)
-        self.__sector_id = sectorid
-        self.__checksum = checksum
 
-    def __validate_checksum(self) -> bool:
-        """Generates the checksum for the words, and returns whether it matches the stored checksum from the input."""
+    def validate(self) -> int:
+        """Generates the checksum for the words, and returns the sector id if it 
+        matches the stored checksum from the input, otherwise zero."""
         letters = ''.join(self.__words)
         lettercount: dict[str, int] = {}
         for c in letters:
@@ -22,56 +26,48 @@ class Room:
                 lettercount[c] = 1
             else:
                 lettercount[c] += 1
-        if len(lettercount) < 5:  # Safety measure, just in case there's less than 5 unique characters in the string
-            return False
-        # Sort by number of occurrences first, alphabetically second, and extract the top 5
-        chksum = ''.join([i[0] for i in sorted(list(lettercount.items()), key=lambda x: (-x[1], x[0]))[:5]])
-        return chksum == self.__checksum
-
-    def get_real_sectorid(self) -> int:
-        """Returns the sector-id of the room if it is valid, otherwise 0."""
-        if self.__validate_checksum():
-            return self.__sector_id
+        if len(lettercount) >= len(self.__checksum):
+            # Sort by number of occurrences first, alphabetically second, and extract the top 5
+            candidate = ''.join([i[0] for i in sorted(list(lettercount.items()), key=lambda x: (-x[1], x[0]))[:len(self.__checksum)]])
+            if candidate == self.__checksum:
+                return self.__sector_id
         return 0
 
-    def decode_room(self) -> str:
-        """Returns the decoded name of the room."""
-        decoded_name = ''
-        for word in self.__words:
-            for c in word:
-                nbr = (ord(c) - ord('a') + self.__sector_id) % (1 + ord('z') - ord('a')) + ord('a')
-                decoded_name += chr(nbr)
-            decoded_name += ' '
-        return decoded_name.strip()  # A bit ugly, but a strip just to remove the last added space at the end...
+    def decode_check(self, target: list[str]) -> bool:
+        """Returns whether the decoded name of the room matches the target."""
+        # 1. Check number of words
+        if len(self.__words) != len(target):
+            return False
+        # 2. Check number of letters in each word
+        for left, right in zip(self.__words, target):
+            if len(left) != len(right):
+                return False
+        # 3. Decode the words and compare to target
+        decoded_name = [''.join([chr((ord(c) - ord('a') + self.__sector_id) % (1 + ord('z') - ord('a')) + ord('a')) for c in word]) for word in self.__words]
+        return decoded_name == target
 
 
-class Kiosk:
-    def __init__(self, rawstr: str) -> None:
-        self.__rooms: list[Room] = []
-        for line in rawstr.splitlines():
-            *words, sec_id, chksm = re.findall(r"\w+", line)
-            self.__rooms.append(Room(words, int(sec_id), chksm))
-        self.__decoded_rooms: dict[str, int] = {}
+class InputData:
+    def __init__(self, s: str) -> None:
+        self.__rooms: list[Room] = [Room(line) for line in s.splitlines()]
 
-    def get_real_rooms_sector_sum(self) -> int:
-        total = 0
+    def solve(self) -> tuple[int, int]:
+        p1 = 0
+        p2 = 0
+        TARGET = ["northpole", "object", "storage"]
         for room in self.__rooms:
-            if (secid := room.get_real_sectorid()) > 0:
-                total += secid
-                self.__decoded_rooms[room.decode_room()] = secid
-        return sum([room.get_real_sectorid() for room in self.__rooms])
-
-    def get_north_pole_sectorid(self) -> int:
-        for room in self.__decoded_rooms:
-            if room == 'northpole object storage':
-                return self.__decoded_rooms[room]
-        return -1
+            s_id = room.validate()
+            p1 += s_id
+            if room.decode_check(TARGET):
+                p2 = s_id
+        return p1, p2
 
 
 def main(aoc_input: str) -> None:
-    kiosk = Kiosk(aoc_input)
-    print(f"Part 1: {kiosk.get_real_rooms_sector_sum()}")
-    print(f"Part 2: {kiosk.get_north_pole_sectorid()}")
+    kiosk = InputData(aoc_input)
+    p1, p2 = kiosk.solve()
+    print(f"Part 1: {p1}")
+    print(f"Part 2: {p2}")
 
 
 if __name__ == "__main__":
