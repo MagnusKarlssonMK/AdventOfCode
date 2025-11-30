3b45bd944bab7b5697c4d6136b2865571cc1bf27
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2025-11-30 17:39:24 +0100

    Add basic plumbing for 2025.

---
+++ 2025/tests/test_day01.py
@@ -0,0 +1,18 @@
+from day01 import Placeholder
+
+# ----------- Part 1 ------------
+
+
+def test_part1_1() -> None:
+    test_string = """ """
+    test_input = Placeholder(test_string)
+    assert test_input.get_p1() == 1
+
+
+# ----------- Part 2 ------------
+
+
+def test_part2_1() -> None:
+    test_string = """ """
+    test_input = Placeholder(test_string)
+    assert test_input.get_p2() == 2
