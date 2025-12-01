72ba8541719f206306cbb8605e81fa1dc9394eac
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2025-12-01 10:53:56 +0100

    Add 2025 day 1.

--- 2025/day01.py
+++ 2025/day01.py
@@ -2,26 +2,45 @@ import time
 from pathlib import Path
 
 
-class Placeholder:
-    def __init__(self, rawstr: str) -> None:
-        pass
+class InputData:
+    def __init__(self, s: str) -> None:
+        self.__rotations = [
+            int(line[1:]) if line[0] == "R" else -int(line[1:])
+            for line in s.splitlines()
+        ]
 
     def get_p1(self) -> int:
-        return 1
+        zero_count = 0
+        dial = 50
+        for v in self.__rotations:
+            dial = (dial + v) % 100
+            if dial == 0:
+                zero_count += 1
+        return zero_count
 
     def get_p2(self) -> int:
-        return 2
+        zero_count = 0
+        dial = 50
+        for v in self.__rotations:
+            if v >= 0:
+                zero_count += (dial + v) // 100
+            elif dial == 0:
+                zero_count += abs(v) // 100
+            else:
+                zero_count += (100 - dial + abs(v)) // 100
+            dial = (dial + v) % 100
+        return zero_count
 
 
 def main(aoc_input: str) -> None:
-    p = Placeholder(aoc_input)
+    p = InputData(aoc_input)
     print(f"Part 1: {p.get_p1()}")
     print(f"Part 2: {p.get_p2()}")
 
 
 if __name__ == "__main__":
     ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
-    INPUT_FILE = Path(ROOT_DIR, "2024/day06.txt")
+    INPUT_FILE = Path(ROOT_DIR, "2025/day01.txt")
 
     start_time = time.perf_counter()
     with open(INPUT_FILE, "r") as file:
--- 2025/tests/test_day01.py
+++ 2025/tests/test_day01.py
@@ -1,18 +1,27 @@
-from day01 import Placeholder
+from day01 import InputData
+
+TEST_STRING = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
 
 # ----------- Part 1 ------------
 
 
 def test_part1_1() -> None:
-    test_string = """ """
-    test_input = Placeholder(test_string)
-    assert test_input.get_p1() == 1
+    test_input = InputData(TEST_STRING)
+    assert test_input.get_p1() == 3
 
 
 # ----------- Part 2 ------------
 
 
 def test_part2_1() -> None:
-    test_string = """ """
-    test_input = Placeholder(test_string)
-    assert test_input.get_p2() == 2
+    test_input = InputData(TEST_STRING)
+    assert test_input.get_p2() == 6
